d800980cdad38c503775c37d8b379a7987dbecaf
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-21 13:07:10 +0000

    Created test case for testing mlfw_mat_string functions

---
+++ ml-framework/testcases/test_mlfw_mat_string.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<mlfw_matrix.h>
+#include<stdlib.h>
+int main()
+{
+	char *str;
+	index_t r,c;
+	dimension_t rows,columns;
+	mlfw_mat_string *matrix;
+	matrix=mlfw_mat_string_from_csv("insurance.csv");
+	if(matrix==NULL)
+	{
+		printf("Low memory or unable to load the file\n");
+		return 0;
+	}
+	mlfw_mat_string_get_dimesnions(matrix,&rows,&columns);
+	for(r=0;r<rows;++r)
+	{
+		for(c=0;c<columns;++c)
+		{
+			mlfw_mat_string_get(matrix,r,c,&str);
+			if(str!=NULL)
+			{
+				printf("%s ",str);
+			}
+		}
+		printf("\n");
+	}
+	mlfw_mat_string_destroy(matrix);
+	return 0;
+}
