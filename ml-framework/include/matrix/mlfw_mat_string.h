67bcd94e1de9f7ef8d9de243e47c7e7bb74551e8
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-21 12:27:46 +0000

    Made required changes

--- ml-framework/include/matrix/mlfw_mat_string.h
+++ ml-framework/include/matrix/mlfw_mat_string.h
@@ -1,25 +1,15 @@
-#ifndef __MLFW__MAT__DOUBLE__
-#define __MLFW__MAT__DOUBLE__
+#ifndef __MLFW__MAT__STRING__
+#define __MLFW__MAT__STRING__
 
-struct __mlfw_mat_double;
-typedef struct __mlfw_mat_double mlfw_mat_double;
-mlfw_mat_double * mlfw_mat_double_create_new(dimension_t rows,dimension_t columns);
-void mlfw_mat_double_destroy(mlfw_mat_double *matrix);
-mlfw_mat_double * mlfw_mat_double_from_csv(const char *csv_file_name);
+struct __mlfw_mat_string;
+typedef struct __mlfw_mat_string mlfw_mat_string;
+mlfw_mat_string * mlfw_mat_string_create_new(dimension_t rows,dimension_t columns);
+void mlfw_mat_string_destroy(mlfw_mat_string *matrix);
+mlfw_mat_string * mlfw_mat_string_from_csv(const char *csv_file_name);
 
-double mlfw_mat_double_get(mlfw_mat_double *matrix,index_t row,index_t column);
-void mlfw_mat_double_set(mlfw_mat_double *matrix,index_t row,index_t column,double value);
+void mlfw_mat_string_get(mlfw_mat_string *matrix,index_t row,index_t column,char **string);
+void mlfw_mat_string_set(mlfw_mat_string *matrix,index_t row,index_t column,char *string);
 
-void mlfw_mat_double_get_dimensions(mlfw_mat_double *matrix,dimension_t *rows,dimension_t *columns);
+void mlfw_mat_string_get_dimensions(mlfw_mat_string *matrix,dimension_t *rows,dimension_t *columns);
 
-void mlfw_mat_double_copy(mlfw_mat_double *target,mlfw_mat_double *source,index_t target_row_index,index_t target_column_index,index_t source_from_row_index,index_t source_from_column_index,index_t source_to_row_index,index_t source_to_column_index);
-
-void mlfw_mat_double_fill(mlfw_mat_double *matrix,index_t from_row_index,index_t from_column_index,index_t to_row_index,index_t to_column_index,double value);
-
-mlfw_column_vec_double * mlfw_mat_double_create_column_vec(mlfw_mat_double *matrix,index_t column_index);
-
-mlfw_mat_double * mlfw_mat_double_shuffle(mlfw_mat_double *matrix,uint8_t how_many_times_to_shuffle);
-void mlfw_mat_double_to_csv(mlfw_mat_double *matrix,const char *csv_file_name);
-
-mlfw_mat_double * mlfw_mat_double_transpose(mlfw_mat_double *matrix);
 #endif
