cb9208029f17cc6522949c3081bd59811b442f1e
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-22 06:19:13 +0000

    Fixed type

--- ml-framework/include/mlfw_set.h
+++ ml-framework/include/mlfw_set.h
@@ -1,10 +1,13 @@
-#ifndef __MLFW__MAT__SET
-#define __MLFW__MAT__SET
+#ifndef __MLFW__SET
+#define __MLFW__SET
+#include<inttypes.h>
+
 
 #ifndef dimension_t
-#define dimension_t uint8_t
-#ifndef index_t
-#define index_t uint8_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+
 
 #include<set/mlfw_set_string.h>
 
