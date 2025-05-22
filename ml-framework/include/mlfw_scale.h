0af215e321e4e99af7cb0ccef7ab62dedaeef82f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-22 08:14:57 +0000

    Added header file

---
+++ ml-framework/include/mlfw_scale.h
@@ -0,0 +1,14 @@
+#ifndef __MLFW__SCALE
+#define __MLFW__SCALE
+#include<inttypes.h>
+
+
+#ifndef dimension_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+
+
+#include<scale/mlfw_scale_double.h>
+
+#endif
