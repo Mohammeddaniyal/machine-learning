0795e85d03e9a32df4514db018022ed80adecbb1
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-22 05:34:11 +0000

    increased header file for new data type

---
+++ ml-framework/include/mlfw_set.h
@@ -0,0 +1,11 @@
+#ifndef __MLFW__MAT__SET
+#define __MLFW__MAT__SET
+
+#ifndef dimension_t
+#define dimension_t uint8_t
+#ifndef index_t
+#define index_t uint8_t
+
+#include<set/mlfw_set_string.h>
+
+#endif
