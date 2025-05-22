1864f668b13b92f83460ef6eb41aa47ced3fe7ce
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-22 07:41:11 +0000

    Made changes

--- ml-framework/include/set/mlfw_set_string.h
+++ ml-framework/include/set/mlfw_set_string.h
@@ -8,7 +8,7 @@ void mlfw_set_string_destroy(mlfw_set_string *set);
 
 void mlfw_set_string_get(mlfw_set_string *set,index_t i,char **string);
 // returns -1 in case of low memory or 0 incase of added or not added because of duplicate
-int  mlfw_set_string_set(mlfw_set_string *set,char *string);
+int  mlfw_set_string_add(mlfw_set_string *set,char *string);
 
 dimension_t mlfw_set_string_get_size(mlfw_set_string *set);
 
