88c95b50ab94a557a7a512b8768955aa6a7e2d8d
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-22 08:34:27 +0000

    Added new compile line for folder scaling

--- ml-framework/o_files/create_lib.sh
+++ ml-framework/o_files/create_lib.sh
@@ -6,4 +6,5 @@ gcc -c ../src/vector/*.c -I ../include -Wall
 gcc -c ../src/operations/*.c -I ../include -Wall
 gcc -c ../src/set/*.c -I ../include -Wall
 gcc -c ../src/encoder/*.c -I ../include -Wall
+gcc -c ../src/scaling/*.c -I ../include -Wall
 ar rcs ../lib/libml-framework.a *.o
