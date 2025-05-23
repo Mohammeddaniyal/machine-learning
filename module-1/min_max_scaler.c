0fc646d0450faf19bea9c1b597450c98d120238c
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-23 14:48:06 +0000

    Made changed refactor the code

--- module-1/min_max_scaler.c
+++ module-1/min_max_scaler.c
@@ -25,13 +25,17 @@ int main(int argc,char *argv[])
 	min_max_file=argv[5];
 
 	matrix=mlfw_mat_double_from_csv(input_file);
-	if(matrix==NULL) return 0;
-
+	if(matrix==NULL)
+	{
+		printf("Low memory\n");
+		return 0;
+	}
 	mlfw_mat_double_get_dimensions(matrix,&matrix_rows,&matrix_columns);
 	
 	scaled_matrix=mlfw_scale_double_min_max(matrix,0,from_column_index,matrix_rows-1,to_column_index,min_max_file);
 	if(scaled_matrix==NULL)
 	{
+		printf("Low memory, while creating scaled matrix\n");
 		mlfw_mat_double_destroy(matrix);
 		return 0;
 	}
