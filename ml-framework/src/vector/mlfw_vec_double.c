f40905b2166653f035e2e3fb0f99b199213fda17
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-23 09:34:06 +0000

    Added implementation for from_csv and to_csv

--- ml-framework/src/vector/mlfw_vec_double.c
+++ ml-framework/src/vector/mlfw_vec_double.c
@@ -91,6 +91,77 @@ double mlfw_column_vec_double_get_mean(mlfw_column_vec_double *vector)
 	return mean;
 }
 
+void mlfw_column_vec_double_to_csv(mlfw_column_vec_double *vector,char *csv_file_name)
+{
+	FILE *file;
+	index_t i;
+	if(vector==NULL || csv_file_name==NULL) return;
+	file=fopen(csv_file_name,"w");
+	if(file==NULL) return;
+	for(i=0;i<vector->size;++i)
+	{
+		fprintf(file,"%lf",vector->data[i]);
+		if(i==vector->size-1) fputc('\n',file);
+		else fputc(',',file);
+	}
+	fclose(file);
+}
+mlfw_column_vec_double * mlfw_column_vec_double_from_csv(char *csv_file_name)
+{
+	mlfw_column_vec_double *vector;
+	FILE *file;
+	index_t i;
+	index_t j;
+	dimension_t size;
+	char double_string[1025];
+	char m;
+	char *end;
+	double value;
+	if(csv_file_name==NULL) return NULL;
+	file=fopen(csv_file_name,"r");
+	if(file==NULL) return NULL;
+	size=0;
+	while(1)
+	{
+		m=fgetc(file);
+		if(feof(file)) break;
+		if(m==',') size++;
+	}
+	size++; // 10 commas means, 11 elements
+	vector=mlfw_column_vec_double_create_new(size);
+	if(vector==NULL)
+	{
+		fclose(file);
+		return NULL;
+	}
+	rewind(file);
+	i=0;
+	j=0;
+	while(1)
+	{
+		m=fgetc(file);
+		if(feof(file)) break;
+		if(m==',' || m=='\n')
+		{
+			double_string[i]='\0';
+			end=NULL;
+			value=strtod(double_string,&end);
+			mlfw_column_vec_double_set(vector,j,value);
+			j++;
+			i=0;
+		}
+		else
+		{
+			double_string[i]=m;
+			i++;
+		}
+
+	}
+	fclose(file);
+	return vector;
+}
+
+
 // row vector function implementations
 
 mlfw_row_vec_double * mlfw_row_vec_double_create_new(dimension_t size)
@@ -162,3 +233,74 @@ double mlfw_row_vec_double_get_mean(mlfw_row_vec_double *vector)
 	mean=sum/(double)vector->size;
 	return mean;
 }
+void mlfw_row_vec_double_to_csv(mlfw_row_vec_double *vector,char *csv_file_name)
+{
+	FILE *file;
+	index_t i;
+	if(vector==NULL || csv_file_name==NULL) return;
+	file=fopen(csv_file_name,"w");
+	if(file==NULL) return;
+	for(i=0;i<vector->size;++i)
+	{
+		fprintf(file,"%lf",vector->data[i]);
+		if(i==vector->size-1) fputc('\n',file);
+		else fputc(',',file);
+	}
+	fclose(file);
+}
+mlfw_row_vec_double * mlfw_row_vec_double_from_csv(char *csv_file_name)
+{
+	mlfw_row_vec_double *vector;
+	FILE *file;
+	index_t i;
+	index_t j;
+	dimension_t size;
+	char double_string[1025];
+	char m;
+	double value;
+	char *end;
+	if(csv_file_name==NULL) return NULL;
+	file=fopen(csv_file_name,"r");
+	if(file==NULL) return NULL;
+	size=0;
+	while(1)
+	{
+		m=fgetc(file);
+		if(feof(file)) break;
+		if(m==',') size++;
+	}
+	size++; // 10 commas means, 11 elements
+	rewind(file);
+	vector=mlfw_row_vec_double_create_new(size);
+	if(vector==NULL)
+	{
+		fclose(file);
+		return NULL;
+	}
+	i=0;
+	j=0;
+	while(1)
+	{
+		m=fgetc(file);
+		if(feof(file)) break;
+		if(m==',' || m=='\n')
+		{
+			double_string[i]='\0';
+			end=NULL;
+			value=strtod(double_string,&end);
+			mlfw_row_vec_double_set(vector,j,value);
+			j++;
+			i=0;
+		}
+		else
+		{
+			double_string[i]=m;
+			i++;
+		}
+
+	}
+	fclose(file);
+	return vector;
+
+}
+
