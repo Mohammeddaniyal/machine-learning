afc2fd688a2ffad814e8ff0a4ef9ebc6ba67152f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-24 09:00:16 +0000

    MedicalChargesPredictorApp.c completed

--- module-1/Insurance/MedicalChargesPredictorApp.c
+++ module-1/Insurance/MedicalChargesPredictorApp.c
@@ -1,10 +1,11 @@
 #include<mlfw_matrix.h>
 #include<mlfw_vector.h>
 #include<mlfw_scale.h>
-#include<mlfw_set.h>
+#include<mlfw_operations.h>
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+
 void clearBuffer()
 {
 	while(getchar()!='\n');
@@ -17,11 +18,8 @@ int main()
 	int number_of_children;
 	char smokes;
 	char region[52];
-	int succ;
 	char found;	
-	mlfw_set_string *regions_set;
-	char *str;
-	dimension_t set_size;
+
 	index_t i;
 
 	double sex_male=0;
@@ -35,9 +33,19 @@ int main()
 	double region_southwest=0;
 	double region_southeast=0;
 
-	char age_string[5];
-	char bmi_string[7];
-	char number_of_children_string[5];
+	
+	mlfw_row_vec_double *I;
+	mlfw_mat_double *min_max_matrix;
+	mlfw_column_vec_double *m;
+	mlfw_column_vec_double *result;
+
+	dimension_t min_max_rows,min_max_columns;
+	index_t c;
+
+	double min,max;
+	double value;
+	double scaled_value;
+
 	printf("Enter age : ");
 	scanf("%d",&age);
 	clearBuffer();
@@ -86,85 +94,113 @@ int main()
 	printf("Enter region : ");
 	fgets(region,52,stdin);
 	region[strlen(region)-1]='\0';
-	regions_set=mlfw_set_string_create_new();
-	if(regions_set==NULL)
-	{
-		printf("Low memory\n");
-		return 0;
-	}
-	succ=mlfw_set_string_add(regions_set,"northeast");
-	if(succ==-1)
-	{
-		printf("Low memory\n");
-		mlfw_set_string_destroy(regions_set);
-		return 0;
-	}
-	succ=mlfw_set_string_add(regions_set,"northwest");
-	if(succ==-1)
-	{
-		printf("Low memory\n");
-		mlfw_set_string_destroy(regions_set);
-		return 0;
-	}
-	succ=mlfw_set_string_add(regions_set,"southeast");
-	if(succ==-1)
-	{
-		printf("Low memory\n");
-		mlfw_set_string_destroy(regions_set);
-		return 0;
-	}
-	succ=mlfw_set_string_add(regions_set,"southwest");
-	if(succ==-1)
-	{
-		printf("Low memory\n");
-		mlfw_set_string_destroy(regions_set);
-		return 0;
-	}
-
-	set_size=mlfw_set_string_get_size(regions_set);
 	found=0;	
 	// lower case the input region
 	for(i=0;region[i];++i)
 	{
 		if(region[i]>=65 && region[i]<=90) region[i]+=32;	
 	}
-	printf("Region %s\n",region);
-	for(i=0;i<set_size;++i)
-	{
-		mlfw_set_string_get(regions_set,i,&str);
-		if(str==NULL)
-		{
-			printf("Low memory\n");
-			mlfw_set_string_destroy(regions_set);
-			return 0;
-		}
-		if(strcmp(region,str)==0) 
-		{	
-			found=1;
-			free(str);
-			break;
-		}
-		free(str);
-	}
-		
+
+	found=0;
+	if(strcmp(region,"northeast")==0)
+	{	
+		found=1;
+		region_northeast=1;
+	}else if(strcmp(region,"southeast")==0)
+	{	
+		found=1;
+		region_southeast=1;
+	}else if(strcmp(region,"northwest")==0)
+	{	
+		found=1;
+		region_northwest=1;
+	}else if(strcmp(region,"southwest")==0)
+	{	
+		found=1;
+		region_southwest=1;
+	}  
+
+
+
 	if(found==0)
 	{
 		printf("Invalid region must be (northeast/northwest/southeast/southwest) \n");
-		mlfw_set_string_destroy(regions_set);
 		return 0;
 	}
 	
-	printf("%d %lf %c %d %c %s\n",age,body_mass_index,gender,number_of_children,smokes,region);
 
-	snprintf(age_string,sizeof(age_string),"%d",age);
-	snprintf(bmi_string,sizeof(bmi_string),"%2.2lf",body_mass_index);
-	snprintf(number_of_children_string,sizeof(number_of_children_string),"%d",number_of_children);
+	I=mlfw_row_vec_double_create_new(12);
+	if(I==NULL)
+	{
+		printf("Low memory\n");
+		return 0;
+	}
 
-	printf("%s %s %s\n",age_string,bmi_string,number_of_children_string);
+	mlfw_row_vec_double_set(I,0,1);
+	mlfw_row_vec_double_set(I,1,(double)age);
+	mlfw_row_vec_double_set(I,2,sex_female);
+	mlfw_row_vec_double_set(I,3,sex_male);
+	mlfw_row_vec_double_set(I,4,body_mass_index);
+	mlfw_row_vec_double_set(I,5,(double)number_of_children);
+	mlfw_row_vec_double_set(I,6,smokes_yes);
+	mlfw_row_vec_double_set(I,7,smokes_no);
+	mlfw_row_vec_double_set(I,8,region_southwest);
+	mlfw_row_vec_double_set(I,9,region_southeast);
+	mlfw_row_vec_double_set(I,10,region_northwest);
+	mlfw_row_vec_double_set(I,11,region_northeast);
+
+
+	min_max_matrix=mlfw_mat_double_from_csv("min_max_scale.csv");
+	if(min_max_matrix==NULL)
+	{
+		printf("Low memory\n");
+		mlfw_row_vec_double_destroy(I);
+		return 0;
+	}
+
+	mlfw_mat_double_get_dimensions(min_max_matrix,&min_max_rows,&min_max_columns);
+	for(c=0,i=1;c<min_max_columns-1;++c,++i)
+	{
+		min=mlfw_mat_double_get(min_max_matrix,0,c);
+		max=mlfw_mat_double_get(min_max_matrix,1,c);
+		value=mlfw_row_vec_double_get(I,i);
+		scaled_value=(value-min)/(max-min);
+		mlfw_row_vec_double_set(I,i,scaled_value);
+	}
 
 	
+	m=mlfw_column_vec_double_from_csv("model.csv");
+	if(m==NULL)
+	{
+		printf("Low memory\n");
+		mlfw_row_vec_double_destroy(I);
+		mlfw_mat_double_destroy(min_max_matrix);
+		return 0;
+	}
 	
+	result=mlfw_multiply_double_row_vector_with_column_vector(I,m);
+	if(result==NULL)
+	{
+		printf("Low memory\n");
+		mlfw_row_vec_double_destroy(I);
+		mlfw_mat_double_destroy(min_max_matrix);
+		mlfw_column_vec_double_destroy(m);	
+		return 0;
+	}
 	
-	mlfw_set_string_destroy(regions_set);
+	// now reverse the scaled the value
+	scaled_value=mlfw_column_vec_double_get(result,0);
+
+	min=mlfw_mat_double_get(min_max_matrix,0,min_max_columns-1);
+	max=mlfw_mat_double_get(min_max_matrix,1,min_max_columns-1);
+
+	value=(scaled_value*(max-min))+min;
+
+	printf("Prediction %lf\n",value);
+
+	mlfw_row_vec_double_destroy(I);
+	mlfw_mat_double_destroy(min_max_matrix);
+	mlfw_column_vec_double_destroy(m);	
+	mlfw_column_vec_double_destroy(result);
 	return 0;
 }
