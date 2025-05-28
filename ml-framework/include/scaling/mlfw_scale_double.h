a85587cbba1a3db36841683b570e82c36afd0f2c
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-28 05:40:56 +0000

    refactor the decalration

--- ml-framework/include/scaling/mlfw_scale_double.h
+++ ml-framework/include/scaling/mlfw_scale_double.h
@@ -4,5 +4,5 @@
 
 mlfw_mat_double * mlfw_scale_double_min_max(mlfw_mat_double *matrix,index_t start_row_index,index_t start_column_index,index_t end_row_index,index_t end_column_index,char *min_max_file,mlfw_mat_double *scaled_matrix);
 
-mlfw_mat_double * mlfw_scale_double_with_given_min_max(mlfw_mat_double *matrix,index_t start_row_index,index_t start_column_index,index_t end_row_index,index_t end_column_index,mlfw_mat_double *min_max_matrix,mlfw_mat_double *reverse_scaled_matrix);
+mlfw_mat_double * mlfw_scale_double_with_given_min_max(mlfw_mat_double *matrix,index_t start_row_index,index_t start_column_index,index_t end_row_index,index_t end_column_index,mlfw_mat_double *min_max_matrix,mlfw_mat_double *scaled_matrix);
 #endif
