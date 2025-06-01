b79e924f2f8fdecd755e3c3eadf840846e40c751
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-01 07:28:30 +0000

    Lost commit history, updated its from_csv and to_csv, optimized the logic(container concept) also added header part seperatly using new data structure

--- ml-framework/include/matrix/mlfw_mat_double.h
+++ ml-framework/include/matrix/mlfw_mat_double.h
@@ -8,7 +8,7 @@ mlfw_mat_double * mlfw_mat_double_create_new(dimension_t rows,dimension_t column
 void mlfw_mat_double_destroy(mlfw_mat_double *matrix);
 // if user pass NULL in 3rd arg so our function will create a new matrix and return it
 // or if not then it will use the arg matrix refrence and return it
-mlfw_mat_double * mlfw_mat_double_from_csv(const char *csv_file_name,mlfw_mat_double *matrix);
+mlfw_mat_double * mlfw_mat_double_from_csv(const char *csv_file_name,mlfw_mat_double *matrix,mlfw_row_vec_string **header);
 
 double mlfw_mat_double_get(mlfw_mat_double *matrix,index_t row,index_t column);
 void mlfw_mat_double_set(mlfw_mat_double *matrix,index_t row,index_t column,double value);
@@ -22,11 +22,17 @@ void mlfw_mat_double_fill(mlfw_mat_double *matrix,index_t from_row_index,index_t
 mlfw_column_vec_double * mlfw_mat_double_create_column_vec(mlfw_mat_double *matrix,index_t column_index,mlfw_column_vec_double *vector);
 
 mlfw_mat_double * mlfw_mat_double_shuffle(mlfw_mat_double *matrix,uint8_t how_many_times_to_shuffle,mlfw_mat_double *shuffled_matrix);
-void mlfw_mat_double_to_csv(mlfw_mat_double *matrix,const char *csv_file_name);
+void mlfw_mat_double_to_csv(mlfw_mat_double *matrix,const char *csv_file_name,mlfw_row_vec_string *header);
 
 mlfw_mat_double * mlfw_mat_double_transpose(mlfw_mat_double *matrix,mlfw_mat_double *transposed_matrix);
 
 double mlfw_mat_double_get_minimum(mlfw_mat_double *matrix,index_t start_row_index,index_t start_column_index,index_t end_row_index,index_t end_column_index);
 
 double mlfw_mat_double_get_maximum(mlfw_mat_double *matrix,index_t start_row_index,index_t start_column_index,index_t end_row_index,index_t end_column_index);
+
+double mlfw_mat_double_get_mean(mlfw_mat_double *matrix,index_t start_row_index,index_t start_column_index,index_t end_row_index,index_t end_column_index);
+
+double mlfw_mat_double_get_standard_deviation(mlfw_mat_double *matrix,index_t start_row_index,index_t start_column_index,index_t end_row_index,index_t end_column_index);
+
+
 #endif
