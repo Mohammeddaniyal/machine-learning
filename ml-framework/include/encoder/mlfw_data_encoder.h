059e9d309b209c599d3c4f57d650915cb450c506
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-02 07:46:18 +0000

    Updated the logic and created more flexible and declared new coding technique binary encoding

--- ml-framework/include/encoder/mlfw_data_encoder.h
+++ ml-framework/include/encoder/mlfw_data_encoder.h
@@ -1,6 +1,6 @@
 #ifndef __MLFW__DATA__ENCODER__
 #define __MLFW__DATA__ENCODER__
-
-void mlfw_encoder_encode_one_hot(char *source,char *target,int *encode_columns,int size,int header_exists);
-
+#include<mlfw_vector.h>
+void mlfw_encoder_encode_one_hot(char *source,char *target,mlfw_row_vec_string *columns_to_encode);
+void mlfw_encoder_encode_binary(char *source,char *target,mlfw_row_vec_string *columns_to_encode);
 #endif
