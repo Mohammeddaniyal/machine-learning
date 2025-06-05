49689368e26b4f52610a973837ba3c5c88a8ee84
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-05 08:15:37 +0000

    Feat implmentation starts of z_score with given mean and std_deviation

--- ml-framework/src/scaling/mlfw_scale_double.c
+++ ml-framework/src/scaling/mlfw_scale_double.c
@@ -236,3 +236,9 @@ mlfw_mat_double * mlfw_scale_double_z_score(mlfw_mat_double *matrix,index_t star
 	return new_matrix;
 
 }
+
+
+mlfw_mat_double * mlfw_scale_double_z_score_with_given_mean_standard_deviation(mlfw_mat_double *matrix,index_t start_row_index,index_t start_column_index,index_t end_row_index,index_t end_column_index,mlfw_mat_double *mean_standard_deviation_matrix,mlfw_mat_double *scaled_matrix)
+{
+
+}
