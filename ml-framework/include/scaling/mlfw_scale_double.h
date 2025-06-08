2c16847a4118cac36c130c957c2f685487b7134e
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-08 08:51:23 +0000

    Increased container parameter in wrapper function

--- ml-framework/include/scaling/mlfw_scale_double.h
+++ ml-framework/include/scaling/mlfw_scale_double.h
@@ -2,9 +2,9 @@
 #define __MLFW__SCALE__DOUBLE__
 #include<mlfw_matrix.h>
 
-mlfw_mat_double * mlfw_scale_double(char *dataset_file_name,mlfw_row_vec_string *columns_to_scale,char *parameters_file_name,char *algorithm,mlfw_mat_double *scaled_matrix);
+mlfw_mat_double * mlfw_scale_double(char *dataset_file_name,mlfw_row_vec_string *columns_to_scale,char *parameters_file_name,char *algorithm,mlfw_mat_double *matrix);
 
-mlfw_mat_double * mlfw_scale_double_with_given_parameters(char *dataset_file_name,char *parameters_file_name,char *algorithm);
+mlfw_mat_double * mlfw_scale_double_with_given_parameters(char *dataset_file_name,char *parameters_file_name,char *algorithm,mlfw_mat_double *matrix);
 
 mlfw_mat_double * mlfw_scale_double_min_max(mlfw_mat_double *matrix,index_t start_row_index,index_t start_column_index,index_t end_row_index,index_t end_column_index,mlfw_mat_double **min_max_matrix,mlfw_mat_double *scaled_matrix);
 
