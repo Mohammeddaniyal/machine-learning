49ab472fe24a6409993f2be5afd58dbf0112f219
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-10 09:22:52 +0000

    Feat new functionalities prototype added for copy and shuffle(string)

--- ml-framework/include/matrix/mlfw_mat_string.h
+++ ml-framework/include/matrix/mlfw_mat_string.h
@@ -14,4 +14,7 @@ void mlfw_mat_string_get_dimensions(mlfw_mat_string *matrix,dimension_t *rows,di
 
 mlfw_mat_string * mlfw_mat_string_transpose(mlfw_mat_string *matrix,mlfw_mat_string *transposed_matrix);
 
+void mlfw_mat_string_copy(mlfw_mat_string *target,mlfw_mat_string *source,index_t target_row_index,index_t target_column_index,index_t source_from_row_index,index_t source_from_column_index,index_t source_to_row_index,index_t source_to_column_index);
+
+mlfw_mat_string * mlfw_mat_string_shuffle(mlfw_mat_string *matrix,uint8_t how_many_times_to_shuffle,mlfw_mat_string *shuffled_matrix);
 #endif
