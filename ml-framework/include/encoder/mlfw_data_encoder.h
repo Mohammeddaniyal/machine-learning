f6b7d0dad7de103c2e39a0ab15606f6575eecf52
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-10 09:02:23 +0000

    Declared protype of a wrapper function

--- ml-framework/include/encoder/mlfw_data_encoder.h
+++ ml-framework/include/encoder/mlfw_data_encoder.h
@@ -3,4 +3,7 @@
 #include<mlfw_vector.h>
 void mlfw_encoder_encode_one_hot(char *source,char *target,mlfw_row_vec_string *columns_to_encode);
 void mlfw_encoder_encode_binary(char *source,char *target,mlfw_row_vec_string *columns_to_encode);
+
+void mlfw_encoder_encode(char *source,char *target,mlfw_row_vec_string *columns_to_encode,char *algorithm);
+
 #endif
