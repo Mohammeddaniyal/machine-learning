22c897ce1416f5ec7b5ff2b116cbc4d704fe1718
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-12 06:39:19 +0000

    Fixed parameter name

--- ml-framework/include/learning/mlfw_score.h
+++ ml-framework/include/learning/mlfw_score.h
@@ -1,5 +1,5 @@
 #ifndef __MLFW__SCORE__
 #define __MLFW__SCORE__
 #include<mlfw_vector.h>
-double get_r2_score(mlfw_column_vec_double *target_values_vector,mlfw_column_vec_double *predicted_values__vector);
+double get_r2_score(mlfw_column_vec_double *target_values_vector,mlfw_column_vec_double *predicted_values_vector);
 #endif
