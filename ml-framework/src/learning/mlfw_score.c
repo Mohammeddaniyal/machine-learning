6ac37539ec3fc151a2f8b590f2c08d1b86a67e5c
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-12 06:26:06 +0000

    Created a structure

---
+++ ml-framework/src/learning/mlfw_score.c
@@ -0,0 +1,9 @@
+#include<mlfw_learning.h>
+#include<mlfw_vector.h>
+#include<mlfw_matrix.h>
+#include<mlfw_operations.h>
+#include<stdio.h>
+double get_r2_score(mlfw_column_vec_double *target_values_vector,mlfw_column_vec_double *predicted_values__vector)
+{
+
+}
