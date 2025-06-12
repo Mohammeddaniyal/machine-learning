f79e71ad26c3d6bcbcb7af6cf1409d288b20351d
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-12 06:17:09 +0000

    Added new header file

---
+++ ml-framework/include/mlfw_learning.h
@@ -0,0 +1,14 @@
+#ifndef __MLFW__LEARNING__
+#define __MLFW__LEARNING__
+#include<inttypes.h>
+
+
+#ifndef dimension_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+
+
+#include<learning/mlfw_learning.h>
+
+#endif
