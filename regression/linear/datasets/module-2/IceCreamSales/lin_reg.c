8e7c09f3e0a991af608cac5f04a7872a46c5068f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-16 06:47:24 +0000

    Fixed missing paranthesis error

--- regression/linear/datasets/module-2/IceCreamSales/lin_reg.c
+++ regression/linear/datasets/module-2/IceCreamSales/lin_reg.c
@@ -16,7 +16,9 @@ int main(int argc,char *argv[])
 	mlfw_row_vec_double *trained_parameters;
 	dimension_t trained_parameters_size;
 	mlfw_mat_double *test_examples_matrix;
+	mlfw_column_vec_double *test_examples_target_values_vector;
 	dimension_t test_examples_matrix_rows,test_examples_matrix_columns;
+	mlfw_column_vec_double *test_examples_predicted_values_vector;
 
 	index_t i;
 	if(argc!=2)
@@ -34,7 +36,7 @@ int main(int argc,char *argv[])
 	}
 
 
-	mlfw_mat_double_get_dimensions(training_examples_matrix,&training_examples_matrix_rows,&trainig_examples_matrix_columns);
+	mlfw_mat_double_get_dimensions(training_examples_matrix,&training_examples_matrix_rows,&training_examples_matrix_columns);
 	mlfw_mat_double_get_dimensions(test_examples_matrix,&test_examples_matrix_rows,&test_examples_matrix_columns);
 	training_examples_target_values_vector=mlfw_mat_double_create_column_vec(training_examples_matrix,1,NULL);
 	if(training_examples_target_values_vector==NULL)
@@ -63,14 +65,56 @@ int main(int argc,char *argv[])
 	}
 	mlfw_mat_double_destroy(training_examples_matrix);
 	mlfw_column_vec_double_destroy(training_examples_target_values_vector);
+	
+	// training parts ends here
+	
 	trained_parameters_size=mlfw_row_vec_double_get_size(trained_parameters);
 	for(i=0;i<trained_parameters_size;++i)
 	{
 		printf("%lf ",mlfw_row_vec_double_get(trained_parameters,i));
 	}
 	
+		printf("\n");
+	// prediction part starts here
+	
+
+	test_examples_target_values_vector=mlfw_mat_double_create_column_vec(test_examples_matrix,1,NULL);
+	if(test_examples_target_values_vector==NULL)
+	{
+		printf("Low memory\n");
+		mlfw_mat_double_destroy(test_examples_matrix);
+		mlfw_row_vec_double_destroy(trained_parameters);
+		return 0;
+	}
+
+	mlfw_mat_double_reshape(&test_examples_matrix,test_examples_matrix_rows,test_examples_matrix_columns-1);
+	if(test_examples_matrix==NULL)
+	{
+		printf("Low memory\n");
+		mlfw_row_vec_double_destroy(trained_parameters);
+		return 0;
+	}
+	test_examples_matrix_columns=test_examples_matrix_columns-1;
+	test_examples_predicted_values_vector=mlfw_linear_regression_predict(test_examples_matrix,trained_parameters);
+	if(test_examples_predicted_values_vector==NULL)
+	{
+		printf("Low memory\n");
+		mlfw_mat_double_destroy(test_examples_matrix);
+		mlfw_column_vec_double_destroy(test_examples_target_values_vector);
+		mlfw_row_vec_double_destroy(trained_parameters);
+		return 0;
+	}
+	
+	// prediction part ends here
+
+
+
+
+	printf("Result is (0-1) : %40.15lf \n",mlfw_get_r2_score(test_examples_target_values_vector,test_examples_predicted_values_vector));
 	
 	mlfw_row_vec_double_destroy(trained_parameters);
+	mlfw_column_vec_double_destroy(test_examples_target_values_vector);
+	mlfw_mat_double_destroy(test_examples_matrix);
 
 	return 0;
 }
