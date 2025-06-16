09fcef1485936f6f046a9013dba0a770f911fa23
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-16 06:29:04 +0000

    Refactored the code

--- regression/linear/datasets/module-2/IceCreamSales/lin_reg.c
+++ regression/linear/datasets/module-2/IceCreamSales/lin_reg.c
@@ -10,53 +10,59 @@ uint8_t screen_logger(uint64_t iteration_number,double error_value)
 }
 int main(int argc,char *argv[])
 {
-	mlfw_mat_double *matrix;
-	mlfw_row_vec_string *matrix_header;
-	dimension_t matrix_rows,matrix_columns;
-	mlfw_column_vec_double *target_values_vector;
+	mlfw_mat_double *training_examples_matrix;
+	dimension_t training_examples_matrix_rows,training_examples_matrix_columns;
+	mlfw_column_vec_double *training_examples_target_values_vector;
 	mlfw_row_vec_double *trained_parameters;
 	dimension_t trained_parameters_size;
+	mlfw_mat_double *test_examples_matrix;
+	dimension_t test_examples_matrix_rows,test_examples_matrix_columns;
+
 	index_t i;
 	if(argc!=2)
 	{
 		printf("Pass numbers of iteration as command line argument\n");
 		return 0;
 	}
-	matrix=mlfw_mat_double_from_csv("IceCreamSales.csv",NULL,&matrix_header);
-	if(matrix==NULL)
-	{
-		printf("Unable to load IceCreamSales.csv\n");
-		return 0;
-	}
-	mlfw_mat_double_get_dimensions(matrix,&matrix_rows,&matrix_columns);
-	target_values_vector=mlfw_mat_double_create_column_vec(matrix,1,NULL);
-	if(target_values_vector==NULL)
+		
+	
+	mlfw_mat_double_get_training_testing_data("IceCreamSales.csv",&training_examples_matrix,&test_examples_matrix,20);
+	if(training_examples_matrix==NULL)
 	{
 		printf("Low memory\n");
-		mlfw_mat_double_destroy(matrix);
-		mlfw_row_vec_string_destroy(matrix_header);
 		return 0;
 	}
-	mlfw_mat_double_reshape(&matrix,matrix_rows,matrix_columns-1);
-	if(matrix==NULL)
-	{
-		printf("Low memory\n");
-		mlfw_mat_double_destroy(matrix);
-		mlfw_row_vec_string_destroy(matrix_header);
-		mlfw_column_vec_double_destroy(target_values_vector);
-		return 0;
-	}
-	matrix_columns=matrix_columns-1;
 
-	trained_parameters=mlfw_linear_regression_gradient_descent_fit_line(matrix,target_values_vector,0.0003,atoi(argv[1]),screen_logger);
+
+	mlfw_mat_double_get_dimensions(training_examples_matrix,&training_examples_matrix_rows,&trainig_examples_matrix_columns);
+	mlfw_mat_double_get_dimensions(test_examples_matrix,&test_examples_matrix_rows,&test_examples_matrix_columns);
+	training_examples_target_values_vector=mlfw_mat_double_create_column_vec(training_examples_matrix,1,NULL);
+	if(training_examples_target_values_vector==NULL)
+	{
+		printf("Low memory\n");
+		mlfw_mat_double_destroy(training_examples_matrix);
+		return 0;
+	}
+	mlfw_mat_double_reshape(&training_examples_matrix,training_examples_matrix_rows,training_examples_matrix_columns-1);
+	if(training_examples_matrix==NULL)
+	{
+		printf("Low memory\n");
+		mlfw_mat_double_destroy(training_examples_matrix);
+		mlfw_column_vec_double_destroy(training_examples_target_values_vector);
+		return 0;
+	}
+	training_examples_matrix_columns=training_examples_matrix_columns-1;
+
+	trained_parameters=mlfw_linear_regression_gradient_descent_fit_line(training_examples_matrix,training_examples_target_values_vector,0.0003,atoi(argv[1]),screen_logger);
 	if(trained_parameters==NULL)
 	{
 		printf("Low memory\n");
-		mlfw_mat_double_destroy(matrix);
-		mlfw_row_vec_string_destroy(matrix_header);
-		mlfw_column_vec_double_destroy(target_values_vector);
+		mlfw_mat_double_destroy(training_examples_matrix);
+		mlfw_column_vec_double_destroy(training_examples_target_values_vector);
 		return 0;
 	}
+	mlfw_mat_double_destroy(training_examples_matrix);
+	mlfw_column_vec_double_destroy(training_examples_target_values_vector);
 	trained_parameters_size=mlfw_row_vec_double_get_size(trained_parameters);
 	for(i=0;i<trained_parameters_size;++i)
 	{
@@ -64,9 +70,6 @@ int main(int argc,char *argv[])
 	}
 	
 	
-	mlfw_mat_double_destroy(matrix);
-	mlfw_row_vec_string_destroy(matrix_header);
-	mlfw_column_vec_double_destroy(target_values_vector);
 	mlfw_row_vec_double_destroy(trained_parameters);
 
 	return 0;
