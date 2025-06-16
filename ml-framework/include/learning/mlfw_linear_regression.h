e078c5ec457a86b96a7b2ddd479cfbfdb6afd951
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-16 06:02:44 +0000

    Increased new prototype for prediction purpose

--- ml-framework/include/learning/mlfw_linear_regression.h
+++ ml-framework/include/learning/mlfw_linear_regression.h
@@ -6,5 +6,6 @@
 
 mlfw_row_vec_double * mlfw_linear_regression_gradient_descent_fit_line(mlfw_mat_double *input_features_matrix,mlfw_column_vec_double *target_values_vector,double learning_rate,uint64_t number_of_iterations,uint8_t (*on_each_iteration) (uint64_t iteration_number,double error_value));
 
+mlfw_column_vec_double * mlfw_linear_regression_predict(mlfw_mat_double *input_features_matrix,mlfw_row_vec_double *trained_parameters);
 
 #endif
