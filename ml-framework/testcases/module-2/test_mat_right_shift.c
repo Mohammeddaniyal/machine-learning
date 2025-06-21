8dd4bd5484e72aa5f81383007ad04bf9f60dc841
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-21 07:52:04 +0000

    Moved the testcases related to there modules

---
+++ ml-framework/testcases/module-2/test_mat_right_shift.c
@@ -0,0 +1,46 @@
+#include<mlfw_matrix.h>
+#include<stdio.h>
+int main()
+{
+	mlfw_mat_double *matrix;
+	dimension_t rows,columns;
+	index_t r,c;
+
+	matrix=mlfw_mat_double_create_new(2,3);
+	if(matrix==NULL)
+	{
+		printf("Low memory\n");
+		return 0;
+	}
+	mlfw_mat_double_set(matrix,0,0,10.33);
+	mlfw_mat_double_set(matrix,0,1,20.33);
+	mlfw_mat_double_set(matrix,0,2,30.33);
+	mlfw_mat_double_set(matrix,1,0,100.33);
+	mlfw_mat_double_set(matrix,1,1,200.33);
+	mlfw_mat_double_set(matrix,1,2,300.33);
+
+
+	mlfw_mat_double_get_dimensions(matrix,&rows,&columns);
+	printf("Before right shift\n");
+	for(r=0;r<rows;++r)
+	{
+		for(c=0;c<columns;++c)
+		{
+			printf("%lf ",mlfw_mat_double_get(matrix,r,c));		
+		}
+		printf("\n");
+	}
+
+	mlfw_mat_double_right_shift(matrix,1);
+	printf("After applying right shift\n");
+	for(r=0;r<rows;++r)
+	{
+		for(c=0;c<columns;++c)
+		{
+			printf("%lf ",mlfw_mat_double_get(matrix,r,c));		
+		}
+		printf("\n");
+	}
+	mlfw_mat_double_destroy(matrix);
+	return 0;
+}
