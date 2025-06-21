d4ab959648469f7746c434af3fc6664e3a6d9905
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-21 07:51:40 +0000

    Moved the testcases related to there modules, but still there are some testcases which are originally created at the time of module-1 but due to refactoring in framework, so the changes also done in module-1 testcases

---
+++ ml-framework/testcases/module-1/test_one_hot_encoding.c
@@ -0,0 +1,7 @@
+#include<mlfw_encoder.h>
+#include<stdio.h>
+int main()
+{
+	int encoded_columns[3]={1,4,5};
+	mlfw_encoder_encode_one_hot("insurance.csv","one_hot_encoded_insurance_.csv",encoded_columns,3,1);
+}
