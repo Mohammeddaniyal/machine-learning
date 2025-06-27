04ec8c1c26207e3e4561975c20112dfdfdc2f039
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-27 09:21:51 +0000

    in gcc compile line added '-O3' so that to optimize the compile and execution

--- ml-framework/o_files/create_lib.sh
+++ ml-framework/o_files/create_lib.sh
@@ -1,12 +1,12 @@
 rm *.o
 rm ../lib/libml-framework.a
 
-gcc -c ../src/matrix/*.c -I ../include -Wall
-gcc -c ../src/vector/*.c -I ../include -Wall
-gcc -c ../src/operations/*.c -I ../include -Wall
-gcc -c ../src/set/*.c -I ../include -Wall
-gcc -c ../src/encoder/*.c -I ../include -Wall
-gcc -c ../src/scaling/*.c -I ../include -Wall
-gcc -c ../src/utils/*.c -I ../include -Wall
-gcc -c ../src/learning/*.c  -I ../include -Wall
+gcc -O3 -c ../src/matrix/*.c -I ../include -Wall
+gcc -O3 -c ../src/vector/*.c -I ../include -Wall
+gcc -O3 -c ../src/operations/*.c -I ../include -Wall
+gcc -O3 -c ../src/set/*.c -I ../include -Wall
+gcc -O3 -c ../src/encoder/*.c -I ../include -Wall
+gcc -O3 -c ../src/scaling/*.c -I ../include -Wall
+gcc -O3 -c ../src/utils/*.c -I ../include -Wall
+gcc -O3 -c ../src/learning/*.c  -I ../include -Wall
 ar rcs ../lib/libml-framework.a *.o
