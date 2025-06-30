8f93a31ae66be7b4028d8412e68bf1fd4929a6c6
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-06-30 06:43:06 +0000

    Fixed name

--- ml-framework/include/operations/mlfw_operations_double.h
+++ ml-framework/include/operations/mlfw_operations_double.h
@@ -16,5 +16,5 @@ mlfw_mat_double * mlfw_subtract_double_matrix_from_matrix(mlfw_mat_double *left_
 
 mlfw_mat_double * mlfw_multiply_double_matrix_with_matrix_strassens(mlfw_mat_double *left_matrix,mlfw_mat_double *right_matrix,mlfw_mat_double *new_matrix);
 
-mlfw_column_vec_double * mlfw_subtract_double_column_vector_from_scalara(mlfw_column_vec_double *vector,double value,mlfw_column_vec_double *new_vector);
+mlfw_column_vec_double * mlfw_subtract_double_column_vector_from_scalar(mlfw_column_vec_double *vector,double value,mlfw_column_vec_double *new_vector);
 #endif
