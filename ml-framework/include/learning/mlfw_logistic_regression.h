456496451202fa5b5e2ad8d9f6d451c27223ba81
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-01 08:32:27 +0000

    Feat new type of regression (logistic)

---
+++ ml-framework/include/learning/mlfw_logistic_regression.h
@@ -0,0 +1,11 @@
+#ifndef __MLFW__LOGISTIC__REGRESSION__
+#define __MLFW__LOGISTIC__REGRESSION__
+#include<mlfw_matrix.h>
+#include<mlfw_vector.h>
+#include<inttypes.h>
+
+mlfw_row_vec_double * mlfw_logistic_regression_gradient_descent_fit(mlfw_mat_double *input_features_matrix,mlfw_column_vec_double *target_values_vector,double learning_rate,uint64_t number_of_iterations,uint8_t (*on_each_iteration) (uint64_t iteration_number,double cost));
+
+mlfw_column_vec_double * mlfw_logistic_regression_predict(mlfw_mat_double *input_features_matrix,mlfw_row_vec_double *trained_parameters);
+
+#endif
