0969f15e4440d7149257506cf174d3af7e0a35d2
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-01 08:57:37 +0000

    Removed ITE

--- ml-framework/src/learning/mlfw_logistic_regression.c
+++ ml-framework/src/learning/mlfw_logistic_regression.c
@@ -118,20 +118,6 @@ mlfw_row_vec_double * mlfw_logistic_regression_gradient_descent_fit(mlfw_mat_dou
 		}
 		
 
-		// ITE=IT*E, IT_rows*E_columns, since E is columns vector IT_rows*1
-		// IT_rows=I_columns
-		ITE=mlfw_column_vec_double_create_new(I_columns);
-		if(ITE==NULL)
-		{
-			mlfw_mat_double_left_shift(I,1);
-			mlfw_mat_double_reshape(&I,I_rows,I_columns-1);
-			mlfw_mat_double_destroy(IT);
-			mlfw_column_vec_double_destroy(m);
-			mlfw_column_vec_double_destroy(P);
-			mlfw_column_vec_double_destroy(SP);
-			mlfw_column_vec_double_destroy(E);
-			return NULL;
-		}
 		TMP=mlfw_column_vec_double_create_new(I_columns);
 		if(TMP==NULL)
 		{
