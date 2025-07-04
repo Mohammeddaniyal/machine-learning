5879c475f0a55b55d0e250dc448872e26a5c2d9d
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-04 05:42:50 +0000

    Feat cmp.sh

--- regression/logistic/src/cmp.sh
+++ regression/logistic/src/cmp.sh
@@ -1,1 +1,1 @@
-gcc $1.c -I ../../../../../ml-framework/include/ -L ../../../../../ml-framework/lib/ -l ml-framework -lm -o $1 -Wall
+gcc $1.c -I ../../../ml-framework/include -L ../../../ml-framework/lib -l ml-framework -o ../build/$1 -Wall 
