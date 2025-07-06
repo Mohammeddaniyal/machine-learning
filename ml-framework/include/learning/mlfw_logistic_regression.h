8a8493be1674a3624507b34f03876b53a914214d
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-06 05:58:33 +0000

    Fixed Forgot to add header file for set in its header file

--- ml-framework/include/learning/mlfw_logistic_regression.h
+++ ml-framework/include/learning/mlfw_logistic_regression.h
@@ -2,7 +2,9 @@
 #define __MLFW__LOGISTIC__REGRESSION__
 #include<mlfw_matrix.h>
 #include<mlfw_vector.h>
+#include<mlfw_set.h>
 #include<inttypes.h>
+
 
 mlfw_row_vec_double * mlfw_logistic_regression_gradient_descent_fit(mlfw_mat_double *input_features_matrix,mlfw_column_vec_double *target_values_vector,double learning_rate,uint64_t number_of_iterations,uint8_t (*on_each_iteration) (uint64_t iteration_number,double cost));
 
