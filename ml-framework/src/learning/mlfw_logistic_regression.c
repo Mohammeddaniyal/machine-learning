0ce5bbb3e9319d244790cc771f16376fdef1fbd6
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-07 04:47:38 +0000

    Fixed

--- ml-framework/src/learning/mlfw_logistic_regression.c
+++ ml-framework/src/learning/mlfw_logistic_regression.c
@@ -617,7 +617,7 @@ mlfw_column_vec_double * mlfw_logistic_regression_multi_class_predict(mlfw_mat_d
 
 	if(input_features_matrix==NULL || trained_parameters_matrix==NULL || class_set==NULL) return NULL;
 	
-	mlfw_mat_double_get_dimensions(trained_parameters_matrix,&trained_parameters_matrix_rows,trained_parameters_matrix_columns);
+	mlfw_mat_double_get_dimensions(trained_parameters_matrix,&trained_parameters_matrix_rows,&trained_parameters_matrix_columns);
 
 	class_set_size=mlfw_set_string_get_size(class_set);
 	if(class_set_size!=trained_parameters_matrix_columns) return NULL;
