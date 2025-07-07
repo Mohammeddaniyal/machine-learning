649fb03040b7bb93c84377ca0c9aa14b2041924b
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-07 04:23:34 +0000

    Forgot to include matrix header file

--- ml-framework/include/math/mlfw_math_operations.h
+++ ml-framework/include/math/mlfw_math_operations.h
@@ -1,6 +1,7 @@
 #ifndef __MLFW__MATH_OPERATIONS__
 #define __MLFW__MATH_OPERATIONS__
 #include<mlfw_vector.h>
+#include<mlfw_matrix.h>
 mlfw_column_vec_double * mlfw_column_vec_double_sigmoid(mlfw_column_vec_double *vector,mlfw_column_vec_double *new_vector);
 
 mlfw_column_vec_double * mlfw_column_vec_double_log(mlfw_column_vec_double *vector,mlfw_column_vec_double *new_vector);
