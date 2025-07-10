0550e06418ee3f024fa90d7157ebfd23c76c3262
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-10 04:48:21 +0000

    Feat mlfw_list_double (forward_list)

---
+++ ml-framework/include/list/mlfw_list_double.h
@@ -0,0 +1,17 @@
+#ifndef __MLFW__LIST__DOUBLE__
+#define __MLFW__LIST__DOUBLE__
+#include<mlfw_vector.h>
+
+struct __mlfw_forward_list_double;
+typedef struct __mlfw_forward_list_double mlfw_forward_list_double;
+
+mlfw_forward_list_double * mlfw_forward_list_double_create_new();
+void mlfw_forward_list_double_destroy();
+
+void mlfw_forward_list_double_insert(mlfw_forward_list_double *forward_list,double value);
+mlfw_row_vec_string * mlfw_forward_list_double_get_row_vector(mlfw_forward_list_double *forward_list);
+
+dimension_t mlfw_forward_list_double_get_size(mlfw_forward_list_double *forward_list);
+void mlfw_forward_list_double_clear(mlfw_forward_list_double *forward_list);
+
+#endif
