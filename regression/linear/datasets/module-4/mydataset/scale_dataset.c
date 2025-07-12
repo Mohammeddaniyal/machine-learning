c6efdad394c367d699217c569e27aed4c834fd70
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-12 16:19:14 +0000

    Feat scale_dataset

--- regression/linear/datasets/module-4/mydataset/scale_dataset.c
+++ regression/linear/datasets/module-4/mydataset/scale_dataset.c
@@ -8,34 +8,47 @@ int main(int argc,char *argv[])
 	mlfw_row_vec_string *columns_to_scale;
 	mlfw_mat_double *matrix;
 	mlfw_row_vec_string *matrix_header;
-	columns_to_scale=mlfw_row_vec_string_create_new(2);
+	char *source;
+	char *target;
+	index_t i;
+	dimension_t columns_to_scale_size;
+	
+	if(argc<=3)
+	{
+		printf("Specify [source csv] [target csv] columns_to_scale ....\n");
+		return 0;
+	}
+	source=argv[1];
+	target=argv[2];
+	matrix=mlfw_mat_double_from_csv(source,NULL,&matrix_header);
+	if(matrix==NULL)
+	{
+		printf("Unable to load %s\n",source);
+		return 0;
+	}
+	mlfw_mat_double_destroy(matrix);
+	columns_to_scale_size=argc-3; // -3 because (program_name,source,target)
+	columns_to_scale=mlfw_row_vec_string_create_new(columns_to_scale_size);
 	if(columns_to_scale==NULL)
 	{
 		printf("Low memory\n");
+		mlfw_row_vec_string_destroy(matrix_header);
 		return 0;
 	}
-	mlfw_row_vec_string_set(columns_to_scale,0,"age");
-	mlfw_row_vec_string_set(columns_to_scale,1,"salary");
-	scaled_matrix=mlfw_scale_double("dummy.csv",columns_to_scale,"mean_sd.csv","z-score",NULL);
+	
+	for(i=3;i<argc;++i)
+	{
+		mlfw_row_vec_string_set(columns_to_scale,i-3,argv[i]);
+	}
+	scaled_matrix=mlfw_scale_double(source,columns_to_scale,"z_score.csv","z-score",NULL);
 	if(scaled_matrix==NULL)
 	{
 		printf("Low memory or some other reason, scaling not done\n");
 		mlfw_row_vec_string_destroy(columns_to_scale);
+		mlfw_row_vec_string_destroy(matrix_header);
 		return 0;
 	}
-	matrix_header=mlfw_row_vec_string_create_new(4);
-	if(matrix_header==NULL)
-	{
-		printf("Low memory\n");
-		mlfw_row_vec_string_destroy(columns_to_scale);
-		mlfw_mat_double_destroy(scaled_matrix);
-		return 0;
-	}
-	mlfw_row_vec_string_set(matrix_header,0,"age");
-	mlfw_row_vec_string_set(matrix_header,1,"height");
-	mlfw_row_vec_string_set(matrix_header,2,"salary");
-	mlfw_row_vec_string_set(matrix_header,3,"incentive");
-	mlfw_mat_double_to_csv(scaled_matrix,"scaled_dummy.csv",matrix_header);
+	mlfw_mat_double_to_csv(scaled_matrix,target,matrix_header);
 	
 	mlfw_row_vec_string_destroy(columns_to_scale);
 	mlfw_mat_double_destroy(scaled_matrix);
