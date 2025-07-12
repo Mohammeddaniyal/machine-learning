ff07bfd51132cadf3780f39a9dc703ec0093ff39
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-12 12:38:47 +0000

    Created own custom dataset generator

---
+++ regression/linear/datasets/module-4/mydataset/dataset_generator.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char *argv[])
+{
+	double x,y;
+	char *target;
+	int start_x,start_y;
+	FILE *file;
+	int j;
+	if(argc!=4)
+	{
+		printf("Specify [target.csv] [start_x] [end_x]\n");
+		return 0;
+	}
+	target=agrv[0];
+	file=fopen(target,"w");
+	fprintf(file,"0,1\n"); // header part
+	start_x=atoi(argv[1]);
+	end_x=atoi(argv[2]);
+	for(j=start_x;j<=end_x;++j)
+	{
+		x=(double)j;
+		y=pow(sin((1.0/15.0*x)),2)+10;
+		fprintf(file,"%lf,%lf\n"x,y);
+	}
+	fclose(file);
+	return 0;
+}
