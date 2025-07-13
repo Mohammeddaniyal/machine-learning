5ab1fdeecdf57f1e235ad641d5f03d796e2172e3
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-13 06:47:06 +0000

    Fixed varibale name

--- regression/linear/datasets/module-4/mydataset/poly_reg.c
+++ regression/linear/datasets/module-4/mydataset/poly_reg.c
@@ -172,7 +172,7 @@ printf("Specify [source csv] [test_predictions csv] [original csv] [learning rat
 	wrapper.matrix=training_examples_matrix;
 	wrapper.target_values_vector=training_examples_target_values_vector;
 	wrapper.learning_rate=learning_rate;
-	wrapper.iteration_number=number_of_iteration;
+	wrapper.iteration_number=number_of_iterations;
 	wrapper.callback=screen_logger;
 	pthread_create(&thread_id,NULL,thread_function,(void *)&wrapper);
 	while(1)
