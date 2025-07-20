d560c1f1d38d71980110b9d56cdcf5925c17d300
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-20 06:04:22 +0000

    Added extern and  commented

--- ml-framework/learn/error-idea/testcases/abc.c
+++ ml-framework/learn/error-idea/testcases/abc.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #include<daniyal.h>
 #include<azka.h>
+// because of the next 2 lines, memory is not going to get allocated for x and msg
+extern int x; // we are saying that x has been declared externally
+extern char msg[512]; // we are saying that msg has been declared externally
 int main()
 {
 	daniyal();
 	azka();
+	printf("Address of global x is %p\n",&x);
+	printf("Address of global msg is %p\n",&msg);
 	return 0;	
 }
