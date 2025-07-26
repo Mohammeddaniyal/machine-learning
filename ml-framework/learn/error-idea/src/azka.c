ee9153d9e7444594e675dff75af4f125f694c0c7
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-26 06:19:53 +0000

    added __thread macro and refactored the azka() function added parameter to represent from which thread the fuction got called

--- ml-framework/learn/error-idea/src/azka.c
+++ ml-framework/learn/error-idea/src/azka.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<daniyal.h>
 // because of the next 2 lines, memory is not going to get allocated for x and msg
-extern int x; // we are saying that x has been declared externally
-extern char msg[512]; // we are saying that msg has been declared externally
-void azka()
+extern __thread int x; // we are saying that x has been declared externally
+extern __thread char msg[512]; // we are saying that msg has been declared externally
+void azka(char *thread)
 {
-	printf("azka - address of global x is %p\n",&x);
-	printf("azka - address of global msg is %p\n",&msg);
+	printf("%s - azka - address of global x is %p\n",thread,&x);
+	printf("%s - azka - address of global msg is %p\n",thread,&msg);
 }
