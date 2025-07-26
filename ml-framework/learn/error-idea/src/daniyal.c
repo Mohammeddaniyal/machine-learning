34a937aa643c463897f72bf642abe903bccf0620
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-26 06:19:38 +0000

    added __thread macro and refactored the daniyal() function added parameter to represent from which thread the fuction got called

--- ml-framework/learn/error-idea/src/daniyal.c
+++ ml-framework/learn/error-idea/src/daniyal.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<daniyal.h>
 // because of the next 2 lines, memory is not going to get allocated for x and msg
-extern int x; // we are saying that x has been declared externally
-extern char msg[512]; // we are saying that msg has been declared externally
-void daniyal()
+extern __thread int x; // we are saying that x has been declared externally
+extern __thread char msg[512]; // we are saying that msg has been declared externally
+void daniyal(char *thread)
 {
-	printf("daniyal - address of global x is %p\n",&x);
-	printf("daniyal - address of global msg is %p\n",&msg);
+	printf("%s - daniyal - address of global x is %p\n",thread,&x);
+	printf("%s - daniyal - address of global msg is %p\n",thread,&msg);
 }
