fd979d2074ab2597397c44afea21be362e39077f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-26 06:30:18 +0000

    fixed inclusion

--- ml-framework/learn/error-idea/src/azka.c
+++ ml-framework/learn/error-idea/src/azka.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<daniyal.h>
+#include<azka.h>
 // because of the next 2 lines, memory is not going to get allocated for x and msg
 extern __thread int x; // we are saying that x has been declared externally
 extern __thread char msg[512]; // we are saying that msg has been declared externally
