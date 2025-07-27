c11f3ae06165aafb3e479b3b9f23628329f51e7b
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-27 03:42:33 +0000

    Introduced macro for invalid index

--- ml-framework/learn/eg1.c
+++ ml-framework/learn/eg1.c
@@ -7,4 +7,5 @@
 #define MLFW_NULL_ARGUMENT_CODE 2
 #define MLFW_NULL_ARGUMENT "null argument against %s"
 
-
+#define MLFW_INVALID_INDEX_CODE 3
+#define MLFW_INVALID_INDEX "Invalid index (%u) against (%s), valid range (%u-%u)"
