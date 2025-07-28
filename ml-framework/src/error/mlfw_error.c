90d443b5c28f912f496630eb0ddd476555645fea
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-28 04:22:18 +0000

    Feat mlfw_error.c

--- ml-framework/src/error/mlfw_error.c
+++ ml-framework/src/error/mlfw_error.c
@@ -1,8 +1,24 @@
 #include<inttypes.h>
 #include<___mlfw_error.h>
 
-uint8_t mlfw_error();
-uint32_t mlfw_get_error_code();
-void mlfw_get_error_string(char *error_string,uint8_t size);
-void mlfw_get_debig_string(char *debig_string,uint_t size);
+uint8_t mlfw_error()
+{
+	return _mlfw_error_code!=0; // !=0 means, error exists
+}
+uint32_t mlfw_get_error_code()
+{
+	return 0;
+}
+void mlfw_get_error_string(char *error_string,uint8_t size)
+{
+}
+void mlfw_get_debug_string(char *debig_string,uint_t size)
+{
+}
+void mlfw_reset_error()
+{
+	_mlfw_error_code=0;
+	_mlfw_error_string[0]='\0';
+	_mlfw_debug_string[0]='\0';
+}
 
