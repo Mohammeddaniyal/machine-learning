62559b3b782d3b9a6a4000eec4f14b8f2390e4a0
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-28 04:26:34 +0000

    updated the create_lib.sh shell file

--- ml-framework/o_files/create_lib.sh
+++ ml-framework/o_files/create_lib.sh
@@ -1,14 +1,14 @@
 rm *.o
 rm ../lib/libml-framework.a
 
-gcc -O3 -c ../src/matrix/*.c -I ../include -Wall
-gcc -O3 -c ../src/vector/*.c -I ../include -Wall
-gcc -O3 -c ../src/operations/*.c -I ../include -Wall
-gcc -O3 -c ../src/set/*.c -I ../include -Wall
-gcc -O3 -c ../src/encoder/*.c -I ../include -Wall
-gcc -O3 -c ../src/scaling/*.c -I ../include -Wall
-gcc -O3 -c ../src/utils/*.c -I ../include -Wall
-gcc -O3 -c ../src/learning/*.c  -I ../include -Wall
-gcc -O3 -c ../src/math/*.c  -I ../include -Wall
-gcc -O3 -c ../src/list/*.c  -I ../include -Wall
+gcc -O3 -c ../src/matrix/*.c -I ../include -I ../private/include  -Wall
+gcc -O3 -c ../src/vector/*.c -I ../include -I ../private/include -Wall
+gcc -O3 -c ../src/operations/*.c -I ../include -I ../private/include -Wall
+gcc -O3 -c ../src/set/*.c -I ../include -I ../private/include -Wall
+gcc -O3 -c ../src/encoder/*.c -I ../include -I ../private/include -Wall
+gcc -O3 -c ../src/scaling/*.c -I ../include -I ../private/include -Wall
+gcc -O3 -c ../src/utils/*.c -I ../include -I ../private/include -Wall
+gcc -O3 -c ../src/learning/*.c  -I ../include -I ../private/include -Wall
+gcc -O3 -c ../src/math/*.c  -I ../include -I ../private/include -Wall
+gcc -O3 -c ../src/list/*.c  -I ../include -I ../private/include -Wall
 ar rcs ../lib/libml-framework.a *.o
