03127f48d04f0218a3869fad254508f360098e5f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-28 04:19:31 +0000

    fixed name

--- ml-framework/private/include/___mlfw_error.h
+++ ml-framework/private/include/___mlfw_error.h
@@ -4,9 +4,9 @@
 #include<inttypes.h>
 
 
-__thread uint32_t mlfw_error_code;
-__thread char mlfw_error_string[512];
-__thread char mlfw_debug_string[512];
+__thread uint32_t _mlfw_error_code;
+__thread char _mlfw_error_string[512];
+__thread char _mlfw_debug_string[512];
 
 #define MLFW_LOW_MEMORY_CODE 1
 #define MLFW_LOW_MEMORY "Insufficient memory, cannot allocate %u bytes"
@@ -20,8 +20,8 @@ __thread char mlfw_debug_string[512];
 void mlfw_reset_error();
 
 #define _mlfw_set_error(code,string,...) \
-	mlfw_error_code=code; \
-	snprintf(mlfw_error_string,511,string,## __VA_ARGS__); \
-	snprintf(mlfw_debug_string,511,"File : %s\nFunction : %s\nLine : %d",__FILE__,__FUNCTION__,__LINE__);
+	_mlfw_error_code=code; \
+	snprintf(_mlfw_error_string,511,string,## __VA_ARGS__); \
+	snprintf(_mlfw_debug_string,511,"File : %s\nFunction : %s\nLine : %d",__FILE__,__FUNCTION__,__LINE__);
 
 #endif
