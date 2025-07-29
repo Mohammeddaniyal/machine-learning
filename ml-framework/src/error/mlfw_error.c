641070092146d469c775a99fc73354d88065a86f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-29 04:23:31 +0000

    Feat get debug string

--- ml-framework/src/error/mlfw_error.c
+++ ml-framework/src/error/mlfw_error.c
@@ -36,8 +36,32 @@ void mlfw_get_error_string(char *error_string,uint8_t size)
 	}
 	*q='\0';
 }
-void mlfw_get_debug_string(char *debig_string,uint_t size)
+void mlfw_get_debug_string(char *debug_string,uint_t size)
 {
+	uint32_t i;
+	uint32_t max_len;
+	char *p;
+	char *q;
+	if(debug_string==null) return;
+	if(size==0) return;
+	if(_mlfw_error_code=0)
+	{
+		debug_string[0]='\0';
+		return;
+	}
+	max_len=size-1;
+	i=0;
+	p=_mlfw_debug_string;
+	q=debug_string;
+	while(*p!='\0' && i<max_len)
+	{
+		*q=*p;
+		++p;
+		++q;
+		++i;
+	}
+	*q='\0';
+
 }
 void mlfw_reset_error()
 {
