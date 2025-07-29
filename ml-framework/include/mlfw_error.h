f3193f3d195801f86593a86f4cf1c2b2b47efdd2
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-29 04:29:19 +0000

    Introduced void with zero parameters functions

--- ml-framework/include/mlfw_error.h
+++ ml-framework/include/mlfw_error.h
@@ -2,10 +2,10 @@
 #define __MLFW__ERROR__
 #include<inttypes.h>
 
-uint8_t mlfw_error();
-uint32_t mlfw_get_error_code();
+uint8_t mlfw_error(void);
+uint32_t mlfw_get_error_code(void);
 void mlfw_get_error_string(char *error_string,uint8_t size);
 void mlfw_get_debug_string(char *debig_string,uint_t size);
-uint32_t mlfw_get_error_string_length();
-uint32_t mlfw_get_debug_string_length();
+uint32_t mlfw_get_error_string_length(void);
+uint32_t mlfw_get_debug_string_length(void);
 #endif
