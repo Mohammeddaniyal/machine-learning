028a8312e4a2cbcf2a1fde916473e049f2ac80a5
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-29 04:16:26 +0000

    Feat declared new function to get length of error string

--- ml-framework/include/mlfw_error.h
+++ ml-framework/include/mlfw_error.h
@@ -6,5 +6,6 @@ uint8_t mlfw_error();
 uint32_t mlfw_get_error_code();
 void mlfw_get_error_string(char *error_string,uint8_t size);
 void mlfw_get_debug_string(char *debig_string,uint_t size);
-
+uint32_t mlfw_get_error_string_length();
+uint32_t mlfw_get_debug_string_length();
 #endif
