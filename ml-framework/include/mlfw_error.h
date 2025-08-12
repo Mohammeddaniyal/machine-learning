8b133656bc4b2668ce8f023989c673c77830c721
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-12 04:52:35 +0000

    Fixed

--- ml-framework/include/mlfw_error.h
+++ ml-framework/include/mlfw_error.h
@@ -5,7 +5,7 @@
 uint8_t mlfw_error(void);
 uint32_t mlfw_get_error_code(void);
 void mlfw_get_error_string(char *error_string,uint8_t size);
-void mlfw_get_debug_string(char *debig_string,uint_t size);
+void mlfw_get_debug_string(char *debug_string,uint8_t size);
 uint32_t mlfw_get_error_string_length(void);
 uint32_t mlfw_get_debug_string_length(void);
 #endif
