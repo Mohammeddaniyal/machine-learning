ef22e9ed2f46c65a0f3a528b19b9d9b1599f037e
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-31 06:40:40 +0000

    Added regularization term added

--- ml-framework/include/learning/mlfw_linear_regression.h
+++ ml-framework/include/learning/mlfw_linear_regression.h
@@ -4,7 +4,7 @@
 #include<mlfw_vector.h>
 #include<inttypes.h>
 
-mlfw_row_vec_double * mlfw_linear_regression_gradient_descent_fit_line(mlfw_mat_double *input_features_matrix,mlfw_column_vec_double *target_values_vector,double learning_rate,uint64_t number_of_iterations,uint8_t (*on_each_iteration) (uint64_t iteration_number,double error_value,mlfw_column_vec_double *predicted_values_vector));
+mlfw_row_vec_double * mlfw_linear_regression_gradient_descent_fit_line(mlfw_mat_double *input_features_matrix,mlfw_column_vec_double *target_values_vector,double learning_rate,double regularization_parameter,uint64_t number_of_iterations,uint8_t (*on_each_iteration) (uint64_t iteration_number,double error_value,mlfw_column_vec_double *predicted_values_vector));
 
 mlfw_column_vec_double * mlfw_linear_regression_predict(mlfw_mat_double *input_features_matrix,mlfw_row_vec_double *trained_parameters);
 
