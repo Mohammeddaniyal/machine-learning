f24faf2514c05423e2d8159a185c123938fd9c55
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-03 05:36:15 +0000

    Moved learning to old

---
+++ ml-framework/include/old/mlfw_learning.h
@@ -0,0 +1,15 @@
+#ifndef __MLFW__LEARNING__
+#define __MLFW__LEARNING__
+#include<inttypes.h>
+
+
+#ifndef dimension_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+
+
+#include<learning/mlfw_score.h>
+#include<learning/mlfw_linear_regression.h>
+#include<learning/mlfw_logistic_regression.h>
+#endif
