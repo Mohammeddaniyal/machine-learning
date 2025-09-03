314150d90e6d54a0399712e4c3ae0795cb879174
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-03 06:49:47 +0000

    Feat mlfw.h

---
+++ ml-framework/include/mlfw.h
@@ -0,0 +1,23 @@
+#ifndef __MLFW__
+#define __MLFW__
+#include<inttypes.h>
+
+
+#ifndef dimension_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+
+#include<mlfw_matrix.h>
+#include<mlfw_operations.h>
+#include<mlfw_vector.h>
+#include<mlfw_encoder.h>
+#include<mlfw_math.h>
+#include<mlfw_utils.h>
+#include<mlfw_error.h>
+#include<mlfw_scale.h>
+#include<mlfw_model_accuracy.h>
+#include<mlfw_set.h>
+#include<mlfw_list.h>
+
+#endif
