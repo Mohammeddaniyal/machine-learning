9248c51a4fd644685c31373f70e1d9dcf9addb56
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-03 05:36:09 +0000

    Moved learning to old

--- ml-framework/include/learning/mlfw_score.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MLFW__SCORE__
-#define __MLFW__SCORE__
-#include<mlfw_vector.h>
-double mlfw_get_r2_score(mlfw_column_vec_double *target_values_vector,mlfw_column_vec_double *predicted_values_vector);
-
-#endif
