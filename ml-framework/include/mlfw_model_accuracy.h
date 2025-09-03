8cd6ff08f97a27b384b21fe7e19479cd7eca4fa8
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-03 05:39:22 +0000

    Created new header file

---
+++ ml-framework/include/mlfw_model_accuracy.h
@@ -0,0 +1,13 @@
+#ifndef __MLFW__MODEL__ACCURACY__
+#define __MLFW__MODEL__ACCURACY__
+#include<inttypes.h>
+
+
+#ifndef dimension_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+
+
+#include<model_accuracy/mlfw_model_accuracy_score.h>
+#endif
