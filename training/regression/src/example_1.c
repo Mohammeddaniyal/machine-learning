3cdfc9f6bba0355a47544c1f1ed8423ca26f004f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-03 06:07:56 +0000

    Created new dir training and example_1 for gradient_descent_options

---
+++ training/regression/src/example_1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<mlfw.h>
+#include<stdlib.h>
+
+#include<___mlfw_error.h>
+extern __thread uint32_t _mlfw_error_code;
+extern __thread char _mlfw_error_string[512];
+extern __thread char _mlfw_debug_string[512];
+
+enum MLFW_GRADIENT_DESCENT_TYPE{MLFW_BATCH_GRADIENT_DESCENT,MLFW_STOCHASTIC_GRADIENT_DESCENT,MLFW_MINI_BATCH_GRADIENT_DESCENT};
+
+typedef struc _mlfw_gradient_descent_options
+{
+	double learning_rate;
+	uint64_t number_of_iterations;
+	int (*progress_callback) (uint64_t iteration_number,void *y,void *predicted_y);
+	int gradient_descent_type;
+	void (*data_provider)(void *x,void *y,uint64_t starting_row_number,uint32_t number_of_rows_to_provide);
+	uint32_t mini_batch_size;
+}mlfw_gradient_descent_options;
+
+mlfw_gradient_descent_options * mlfw_gradient_descent_options_create_new()
+{
+	mlfw_gradient_descent_options *gd_options;
+	mlfw_reset_error();
+	gd_options=(mlfw_gradient_descent_options *)malloc(sizeof(mlfw_gradient_descent_options));
+	if(gd_options==NULL)
+	{
+		_mlfw_set_error(MLFW_LOW_MEMORY_CODE,MLFW_LOW_MEMORY,sizeof(mlfw_gradient_descent_options));
+		return NULL;
+	}
+	gd_options->learning_rate=0.0;
+	gd_options->number_of_iterations=0;
+	gd_options->progress_callback=NULL;
+	gd_options->gradient_descent_type=-1;
+	gd_options->data_provider=NULL;
+	gd_options->mini_batch_size=0;
+	return gd_options;
+}
