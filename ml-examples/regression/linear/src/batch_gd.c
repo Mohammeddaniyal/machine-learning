8dd1bc5260e0a107693213b7bb77f59e813ce677
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-04 09:18:07 +0000

    Feat getter for data_provider

--- ml-examples/regression/linear/src/batch_gd.c
+++ ml-examples/regression/linear/src/batch_gd.c
@@ -163,6 +163,16 @@ mlfw_gradient_descent_lin_reg_progress_call_t mlfw_gradient_descent_options_get_
 	}
 	return gd_options->progress_callback;
 }
+mlfw_gradient_descent_lin_reg_data_provider_t mlfw_gradient_descent_options_get_number_of_iterations(mlfw_gradient_descent_options *gradient_descent)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return 0;
+	}
+	return gd_options->data_provider;
+}
 mlfw_column_vec_double * mlfw_linear_regression_fit_using_batch_gradient_descent(mlfw_gradient_descent_options  *gd_options,mlfw_mat_double *x,mlfw_column_vec_double *y,double regularization_parameter,mlfw_column_vec_double *model)
 {
 
