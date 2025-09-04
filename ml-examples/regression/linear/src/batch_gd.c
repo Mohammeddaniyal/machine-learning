25006df8d94d119cf8fae35445014d09f98b0263
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-04 09:20:00 +0000

    Feat getter for mini_batch_size

--- ml-examples/regression/linear/src/batch_gd.c
+++ ml-examples/regression/linear/src/batch_gd.c
@@ -153,7 +153,7 @@ int mlfw_gradient_descent_options_get_gradient_descent_type(mlfw_gradient_descen
 	}
 	return gd_options->gradient_descent_type;
 }
-mlfw_gradient_descent_lin_reg_progress_call_t mlfw_gradient_descent_options_get_number_of_iterations(mlfw_gradient_descent_options *gradient_descent)
+mlfw_gradient_descent_lin_reg_progress_call_t mlfw_gradient_descent_options_get_progress_callback(mlfw_gradient_descent_options *gradient_descent)
 {
 	mlfw_reset_error();
 	if(gd_options==NULL)
@@ -163,7 +163,7 @@ mlfw_gradient_descent_lin_reg_progress_call_t mlfw_gradient_descent_options_get_
 	}
 	return gd_options->progress_callback;
 }
-mlfw_gradient_descent_lin_reg_data_provider_t mlfw_gradient_descent_options_get_number_of_iterations(mlfw_gradient_descent_options *gradient_descent)
+mlfw_gradient_descent_lin_reg_data_provider_t mlfw_gradient_descent_options_get_data_provider(mlfw_gradient_descent_options *gradient_descent)
 {
 	mlfw_reset_error();
 	if(gd_options==NULL)
@@ -172,6 +172,16 @@ mlfw_gradient_descent_lin_reg_data_provider_t mlfw_gradient_descent_options_get_
 		return 0;
 	}
 	return gd_options->data_provider;
+}
+uint32_t mlfw_gradient_descent_options_get_mini_batch_size(mlfw_gradient_descent_options *gradient_descent)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return 0;
+	}
+	return gd_options->mini_batch_size;
 }
 mlfw_column_vec_double * mlfw_linear_regression_fit_using_batch_gradient_descent(mlfw_gradient_descent_options  *gd_options,mlfw_mat_double *x,mlfw_column_vec_double *y,double regularization_parameter,mlfw_column_vec_double *model)
 {
