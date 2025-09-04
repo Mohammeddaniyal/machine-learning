8106abb1f6e3959b7571d377d125bfc17fb0b1a8
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-04 11:19:00 +0000

    Feat on_iteration_complete

--- ml-examples/regression/linear/src/batch_gd.c
+++ ml-examples/regression/linear/src/batch_gd.c
@@ -377,10 +377,171 @@ void load_dataset(mlfw_mat_double **x,mlfw_column_vec_double **y)
 	mlfw_mat_double_fill(matrix,0,0,matrix_rows-1,0,1.0);
 	*x=matrix;
 }
-int on_iteration_complete(uint64_t iteration_number,void *y,void *predicted_y)
+int on_iteration_complete(uint64_t iteration_number,void *y,void *predicted_y,void *model,double regularization_parameter)
 {
-	printf("Iteration : %" PRIu64 "\n",iteration_number);
-	return 1; // keep running
+	static mlfw_column_vec_double *prediction_error=NULL;
+	static mlfw_row_vec_double *prediction_error_transposed=NULL;
+	static mlfw_column_vec_double *product_vector=NULL;
+	static mlfw_row_vec_double *model_transposed=NULL;
+	static mlfw_column_vec_double *model_squared_sum_vector=NULL;
+
+	FILE *cost_file;
+	FILE *line_fit_file;
+	double cost;
+	double model_0; // to store value of model[0] before setting to 0
+			// as it is to be ignore in regularization term
+	double regularization_term;
+	double model_squared_sum;
+
+	double sum_of_squared_errors;
+	dimension_t y_size;
+	dimension_t model_size;
+	// for line fitting graph using (y=mx+c)	
+	double x1,x2,y1,y2;
+	double m,c;
+
+	if(y==NULL)
+	{
+		printf("y vector is NULL\n");
+		return -1;
+	}
+	if(predicted_y==NULL)
+	{
+		printf("predicted_y vector is NULL\n");
+		return -1;
+	}
+	if(model==NULL)
+	{
+		printf("model vector is NULL\n");
+		return -1;
+	}
+	y_size=mlfw_column_vec_double_get_size(y);
+	model_size=mlfw_column_vec_double_get_size(model);
+
+	if(iteration_number==1)
+	{
+		// one time act, at the start
+		if(SHOW_GRAPH)
+		{
+			gnuplot=popen("gnuplot","w");
+			fprintf(gnuplot,"set datafile separator ',''n");
+			fflush(gnuplot);
+			/* [1:25] How ?
+			 * After analyzing the cost initially without displaying
+			 * the cost graph, this 1:25 was decided, the set the
+			 * SHOW_COST_GRAPH to 1 and frequency to 100000
+			 */
+			cost_file=fopen("cost.csv","w");
+			fclose(cost_file); // ensure cost.csv is empty
+		}
+		prediction_error=mlfw_column_vec_double_create_new(y_size);
+		if(mlfw_error()) goto err;
+		prediction_error_transposed=mlfw_row_vec_double_create_new(y_size);
+		if(mlfw_error()) goto err;
+		product_vector=mlfw_column_vec_double_create_new(1);
+		if(mlfw_error()) goto err;
+		model_transposed=mlfw_row_vec_double_create_new(model_size);
+		if(mlfw_error()) goto err;
+		model_squared_sum_vector=mlfw_column_vec_double_create_new(1);
+		if(mlfw_error()) goto err;
+	}
+	// calculate error
+	mlfw_subtract_double_column_vector(predicted_y,y,prediction_error);
+	if(mlfw_error()) goto err;
+	// calculate error squared sum
+	mlfw_column_vec_double_transpose(prediction_error,prediction_error_transposed);
+	if(mlfw_error()) goto err;
+	mlfw_multiply_double_row_vector_with_column_vector(prediction_error_transposed,prediction_error,product_error);
+	if(mlfw_error()) goto err;
+	sum_of_squared_errors=mlfw_column_vec_double_get(product_vector,0);
+
+	// pick model[0] and set model[0] to 0 as we want to ignore
+	// bias while calculating regularization_term
+	model_0=mlfw_column_vec_double_get(model,0);
+	mlfw_column_vec_double_set(model,0,0.0);
+	mlfw_column_vec_double_tranpose(model,model_transposed);
+	if(mlfw_error())
+	{
+		mlfw_column_vec_double_set(model,0,model_0);
+		goto err;
+	}
+	// calculate model/theta squared sum
+	mlfw_multiplY_double_row_vector_with_column_vector(model_transposed,model,model_squared_sum_vector);
+	mlfw_column_vec_double_set(model,0,model_0);
+	if(mlfw_error()) goto err;
+	model_squared_sum=mlfw_column_vec_double_get(model_squared_sum_vector,0);
+	// calculate regularization term
+	regularization_term=(regularization_parameter/(2*y_size))*model_squared_sum;
+
+	// calculate cost
+	cost=(sum_of_squared_errors/(2*y_size))+regularization_term;
+	if(SHOW_GRAPH)
+	{
+		if(gnuplot!=NULL)
+		{
+			cost_file=fopen("cost.csv","a");
+			fprintf(cost_file,"%lu,%f\n",iteration_number,cost);
+			fclose(cost_file);
+		}
+	}
+	if(iteration_number%FREQUENCY_OF_PRINTING_COST==0 || iteration_number==NUMBER_OF_ITERATIONS)
+	{
+		printf("Iteration number %lu, Cost %41.15lf\n",iteration_number,cost);
+		if(SHOW_GRAPH)
+		{
+			if(gnuplot!=NULL)
+			{
+				fprintf(gnuplot,"set term x11 0 position 100,100 title 'Cost Descent\n");
+				fflush(gnuplot);
+				fprintf(gnuplot,"set yrange[1:25]\n");
+				fflush(gnuplot);
+				fprintf(gnuplot,"set xrange[1:%u]\n",NUMBER_OF_ITERATIONS);
+				fflush(gnuplot);
+				fprintf(gnuplot,"plot 'cost.csv' notitle\n");
+				fflush(gnuplot);
+				// y=mx+c
+				c=mlfw_column_vec_double_get(model,0);
+				m=mlfw_column_vec_double_get(model,1);
+				x1=0.0;
+				y1=m*x1+c;
+				x2=110.0;
+				y2=m*x2+c;
+				line_fit_file=fopen("line.csv","w");
+				fprintf(line_fit_file,"%lf,%lf\n",x1,y1);
+				fprintf(line_fit_file,"%lf,%lf\n",x2,y2);
+				fclose(line_fit_file);
+				fprintf(gnuplot,"set term x11 1 position 800,100 title 'Line Fitting'\n");
+				fflush(gnuplot);
+				// How [40:110] [10:110] decided ?
+				// plot the data file using externally and analyze the generated
+				// x and y range
+				fprintf(gnuplot,"set xrange[40:110]\n");
+				fflush(gnuplot);
+				fprintf(gnuplot,"set yrange[10:110]\n");
+				fflush(gnuplot);
+				fprintf(gnuplot,"plot '%s' skip 1 notitle, 'line.csv' with lines lw 3 notitle\n",TRAINING_DATASET);
+				fflush(gnuplot);
+			}
+		}
+	}
+	if(iteration_number==NUMBER_OF_ITERATIONS)
+	{
+		// one time act, at the end
+		mlfw_column_vec_double_destroy(prediction_error);
+		mlfw_row_vec_double_destroy(prediction_error_transposed);
+		mlfw_column_vec_double_destroy(product_vector);
+		mlfw_row_vec_double_destroy(model_transposed);
+		mlfw_column_vec_double_destroy(model_squared_sum_vector);
+	}
+	return 0;
+
+	err:
+		mlfw_column_vec_double_destroy(prediction_error);
+		mlfw_row_vec_double_destroy(prediction_error_transposed);
+		mlfw_column_vec_double_destroy(product_vector);
+		mlfw_row_vec_double_destroy(model_transposed);
+		mlfw_column_vec_double_destroy(model_squared_sum_vector);
+		return -1;
 }
 mlfw_gradient_descent_options * get_gradient_descent_options()
 {
