9bf196b1ee4fd81176630847dacbb13ab7a4b330
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-04 09:11:47 +0000

    Removed the existing code in linear_reg fit

---
+++ ml-examples/regression/linear/src/cmp.sh
@@ -0,0 +1,4 @@
+#after moving the framework oriented functions to appropriate framework folders
+#remove the -I ../../../ml-framework/private/include
+
+gcc $1.c -I ../../../ml-framework/include -I ../../../ml-framework/private/include -L ../../../ml-framework/lib -l ml-framework -lm -o ../build/$1 -Wall
