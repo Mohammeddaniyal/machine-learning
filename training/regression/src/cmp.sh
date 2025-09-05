46dba643859778b5e1d7c7a82b61bd1be5faefa4
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-05 05:06:40 +0000

    Renamed the training to ml-examples

--- training/regression/src/cmp.sh
+++
@@ -1,4 +0,0 @@
-#after moving the framework oriented functions to appropriate framework folders
-#remove the -I ../../../ml-framework/private/include
-
-gcc $1.c -I ../../../ml-framework/include -I ../../../ml-framework/private/include -L ../../../ml-framework/lib -l ml-framework -lm -o ../build/$1 -Wall
