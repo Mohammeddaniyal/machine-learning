a7598127bbc5564193467d4beb4f95509714960e
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-06 04:42:42 +0000

    Refactored the file name

---
+++ ml-framework/include/mlfw_algorithms.h
@@ -0,0 +1,15 @@
+#ifndef __MLFW__ALGORITHM__
+#define __MLFW__ALGORITHM__
+#include<inttypes.h>
+
+
+#ifndef dimension_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+
+
+#include<ml_algorithms/mlfw_ml_configurations.h>
+#include<ml_algorithms/mlfw_linear_regression.h>
+
+#endif
