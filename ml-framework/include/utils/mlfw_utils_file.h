dc235addc0271a2ff733dbfa243d79af54b6894a
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-06 04:43:16 +0000

    Fixed parameter type

--- ml-framework/include/utils/mlfw_utils_file.h
+++ ml-framework/include/utils/mlfw_utils_file.h
@@ -3,5 +3,5 @@
 #include<inttypes.h>
 uint64_t mlfw_get_csv_rows_count(const char *csv_file_name);
 uint64_t mlfw_get_csv_columns_count(const char *csv_file_name);
-void mlfw_get_csv_dimensions(const char *csv_file_name,uint64_t *rows,uint64_t columns);
+void mlfw_get_csv_dimensions(const char *csv_file_name,uint64_t *rows,uint64_t *columns);
 #endif
