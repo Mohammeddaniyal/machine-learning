ab3613e5324c6bad92beba5e5c2cb8843e8dd044
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-06 05:04:16 +0000

    Feat mlfw_ml_configurations.c

---
+++ ml-framework/src/ml_algorithms/mlfw_ml_configurations.c
@@ -0,0 +1,180 @@
+#include<stdio.h>
+#include<mlfw_ml_configurations.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<___mlfw_error.h>
+extern __thread uint32_t _mlfw_error_code;
+extern __thread char _mlfw_error_string[512];
+extern __thread char _mlfw_debug_string[512];
+
+
+typedef struct _mlfw_gradient_descent_options
+{
+	double learning_rate;
+	uint64_t number_of_iterations;
+	mlfw_gradient_descent_lin_reg_progress_callback_t progress_callback;
+	int gradient_descent_type;
+	mlfw_gradient_descent_lin_reg_data_provider_t data_provider;
+	uint32_t mini_batch_size;
+}mlfw_gradient_descent_options;
+
+mlfw_gradient_descent_options * mlfw_gradient_descent_options_create_new()
+{
+	mlfw_gradient_descent_options *gd_options;
+	mlfw_reset_error();
+	gd_options=(mlfw_gradient_descent_options *)malloc(sizeof(mlfw_gradient_descent_options));
+	if(gd_options==NULL)
+	{
+		_mlfw_set_error(MLFW_LOW_MEMORY_CODE,MLFW_LOW_MEMORY,sizeof(mlfw_gradient_descent_options));
+		return NULL;
+	}
+	gd_options->learning_rate=0.0;
+	gd_options->number_of_iterations=0;
+	gd_options->progress_callback=NULL;
+	gd_options->gradient_descent_type=-1;
+	gd_options->data_provider=NULL;
+	gd_options->mini_batch_size=0;
+	return gd_options;
+}
+void mlfw_gradient_descent_options_destroy(mlfw_gradient_descent_options *gd_options)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return;
+	}
+	free(gd_options);
+}
+void mlfw_gradient_descent_options_set_learning_rate(mlfw_gradient_descent_options *gd_options,double learning_rate)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{	
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return;
+	}
+	gd_options->learning_rate=learning_rate;
+}
+void mlfw_gradient_descent_options_set_number_of_iterations(mlfw_gradient_descent_options *gd_options,uint64_t number_of_iterations)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{	
+		return;
+	}
+	gd_options->number_of_iterations=number_of_iterations;
+}
+void mlfw_gradient_descent_options_set_gradient_descent_type(mlfw_gradient_descent_options *gd_options,int gradient_descent_type)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{	
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return;
+	}
+	if(gradient_descent_type==MLFW_BATCH_GRADIENT_DESCENT)
+	{
+	gd_options->gradient_descent_type=MLFW_BATCH_GRADIENT_DESCENT;
+	}else if(gradient_descent_type==MLFW_STOCHASTIC_GRADIENT_DESCENT)
+	{
+	gd_options->gradient_descent_type=MLFW_STOCHASTIC_GRADIENT_DESCENT;
+	}else if(gradient_descent_type==MLFW_MINI_BATCH_GRADIENT_DESCENT)
+	{
+	gd_options->gradient_descent_type=MLFW_MINI_BATCH_GRADIENT_DESCENT;
+	}else
+	{
+	_mlfw_set_error(MLFW_INVALID_GRADIENT_DESCENT_TYPE_CODE,MLFW_INVALID_GRADIENT_DESCENT_TYPE);
+	}
+}
+void mlfw_gradient_descent_options_set_progress_callback(mlfw_gradient_descent_options *gd_options,mlfw_gradient_descent_lin_reg_progress_callback_t progress_callback)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{	
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return;
+	}
+	gd_options->progress_callback=progress_callback;
+}
+void mlfw_gradient_descent_options_set_data_provider(mlfw_gradient_descent_options *gd_options,mlfw_gradient_descent_lin_reg_data_provider_t data_provider)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{	
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return;
+	}
+	gd_options->data_provider=data_provider;
+}
+void mlfw_gradient_descent_options_set_mini_batch_size(mlfw_gradient_descent_options *gd_options,uint32_t mini_batch_size)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{	
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return;
+	}
+	gd_options->mini_batch_size=mini_batch_size;
+}
+double mlfw_gradient_descent_options_get_learning_rate(mlfw_gradient_descent_options *gd_options)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return 0.0;
+	}
+	return gd_options->learning_rate;
+}
+uint64_t mlfw_gradient_descent_options_get_number_of_iterations(mlfw_gradient_descent_options *gd_options)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return 0;
+	}
+	return gd_options->number_of_iterations;
+}
+int mlfw_gradient_descent_options_get_gradient_descent_type(mlfw_gradient_descent_options *gd_options)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return 0;
+	}
+	return gd_options->gradient_descent_type;
+}
+mlfw_gradient_descent_lin_reg_progress_callback_t mlfw_gradient_descent_options_get_progress_callback(mlfw_gradient_descent_options *gd_options)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return 0;
+	}
+	return gd_options->progress_callback;
+}
+mlfw_gradient_descent_lin_reg_data_provider_t mlfw_gradient_descent_options_get_data_provider(mlfw_gradient_descent_options *gd_options)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return 0;
+	}
+	return gd_options->data_provider;
+}
+uint32_t mlfw_gradient_descent_options_get_mini_batch_size(mlfw_gradient_descent_options *gd_options)
+{
+	mlfw_reset_error();
+	if(gd_options==NULL)
+	{
+		_mlfw_set_error(MLFW_NULL_ARGUMENT_CODE,MLFW_NULL_ARGUMENT,"gd_options");
+		return 0;
+	}
+	return gd_options->mini_batch_size;
+}
+
