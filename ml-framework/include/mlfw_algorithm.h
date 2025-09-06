082e5a8bca249258d7a8a002de7fa6b19ee71dd9
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-06 04:39:45 +0000

    Feat new header file

---
+++ ml-framework/include/mlfw_algorithm.h
@@ -0,0 +1,15 @@
+#ifndef __MLFW__ALGORITHM__
+#define __MLFW__ALGORITHM__
+#include<inttypes.h>
+
+
+#ifndef dimension_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+
+
+#include<ml-algorithm/mlfw_configuration.h>
+#include<ml-algorithm/mlfw_linear_regression.h>
+
+#endif
