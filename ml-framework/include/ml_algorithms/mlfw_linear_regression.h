371cc011cf430d74dbfca6082c4ab94839e079bc
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-06 05:00:02 +0000

    Feat mlfw_linear_regression.h

--- ml-framework/include/ml_algorithms/mlfw_linear_regression.h
+++ ml-framework/include/ml_algorithms/mlfw_linear_regression.h
@@ -1,4 +1,10 @@
-#ifndef __MLFW__ML_CONFIGURATIONS__
-#define __MLFW__ML_CONFIGURATIONS__
+#ifndef __MLFW__LINEAR_REGRESSION__
+#define __MLFW__LINEAR_REGRESSION__
+#include<mlfw_vector.h>
+#include<mlfw_ml_configurations.h>
+#include<mlfw_vector.h>
 
+mlfw_column_vec_double * mlfw_linear_regression_fit_using_batch_gradient_descent(mlfw_gradient_descent_options  *gd_options,mlfw_mat_double *x,mlfw_column_vec_double *y,double regularization_parameter,mlfw_column_vec_double *model);
+
+mlfw_column_vec_double * mlfw_linear_regression_predict(mlfw_mat_double *x,mlfw_column_vec_double *model);
 #endif
