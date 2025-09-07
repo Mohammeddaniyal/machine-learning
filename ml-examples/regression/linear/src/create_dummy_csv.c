b1b11dd9f8f36d8839090cbcffd2f890008c92ad
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-07 05:47:38 +0000

    Feat

---
+++ ml-examples/regression/linear/src/create_dummy_csv.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	double a,b;
+	FILE *t;
+	t=fopen("test.csv","w");
+	fprintf(t,"col-1,col-2\n");
+	a=1.0;
+	while(a<=1500)
+	{
+		fprintf(t,"%lf,%lf\n",a,a);
+		++a;
+	}
+	fclose(t);
+	return 0;
+}
