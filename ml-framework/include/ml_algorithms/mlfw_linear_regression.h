9deb911feee5f5b48e56a16eefbfe6046877f15f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-08 14:45:46 +0000

    Feat

--- ml-framework/include/ml_algorithms/mlfw_linear_regression.h
+++ ml-framework/include/ml_algorithms/mlfw_linear_regression.h
@@ -6,4 +6,7 @@
 mlfw_column_vec_double * mlfw_linear_regression_fit_using_batch_gradient_descent(mlfw_gradient_descent_options  *gd_options,mlfw_mat_double *x,mlfw_column_vec_double *y,double regularization_parameter,mlfw_column_vec_double *model);
 
 mlfw_column_vec_double * mlfw_linear_regression_predict(mlfw_mat_double *x,mlfw_column_vec_double *model);
+
+mlfw_column_vec_double * mlfw_linear_regression_fit_using_stochastic_gradient_descent(mlfw_gradient_descent_options *gd_options,double regularization_parameter,mlfw_column_vec_double *model);
+
 #endif
