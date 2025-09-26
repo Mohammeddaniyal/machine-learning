25d9020902eb459bcdb8371b361961e77a285617
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:09:50 -0700

    Replace prefix 'mlfw' with 'dmlfw' in min_max_scaler.c

--- tools/src/min_max_scaler.c
+++ tools/src/min_max_scaler.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<mlfw_scale.h>
-#include<mlfw_matrix.h>
-#include<mlfw_vector.h>
+#include<dmlfw_scale.h>
+#include<dmlfw_matrix.h>
+#include<dmlfw_vector.h>
 int main(int argc,char *argv[])
 {
 
-	mlfw_mat_double *matrix;
-	mlfw_mat_double *scaled_matrix;
-	mlfw_row_vec_string *header;
+	dmlfw_mat_double *matrix;
+	dmlfw_mat_double *scaled_matrix;
+	dmlfw_row_vec_string *header;
 	dimension_t matrix_rows,matrix_columns;
 
 	char *input_file;
@@ -27,28 +27,28 @@ int main(int argc,char *argv[])
 	to_column_index=atoi(argv[4]);
 	min_max_file=argv[5];
 
-	matrix=mlfw_mat_double_from_csv(input_file,NULL,&header);
+	matrix=dmlfw_mat_double_from_csv(input_file,NULL,&header);
 	if(matrix==NULL)
 	{
 		printf("Low memory\n");
 		return 0;
 	}
-	mlfw_mat_double_get_dimensions(matrix,&matrix_rows,&matrix_columns);
+	dmlfw_mat_double_get_dimensions(matrix,&matrix_rows,&matrix_columns);
 	
-	scaled_matrix=mlfw_scale_double_min_max(matrix,0,from_column_index,matrix_rows-1,to_column_index,min_max_file,NULL);
+	scaled_matrix=dmlfw_scale_double_min_max(matrix,0,from_column_index,matrix_rows-1,to_column_index,min_max_file,NULL);
 	if(scaled_matrix==NULL)
 	{
 		printf("Low memory, while creating scaled matrix\n");
-		mlfw_mat_double_destroy(matrix);
-		mlfw_row_vec_string_destroy(header);
+		dmlfw_mat_double_destroy(matrix);
+		dmlfw_row_vec_string_destroy(header);
 		return 0;
 	}
 	
-	mlfw_mat_double_to_csv(scaled_matrix,output_file,header);
+	dmlfw_mat_double_to_csv(scaled_matrix,output_file,header);
 		
-	mlfw_mat_double_destroy(matrix);
-	mlfw_row_vec_string_destroy(header);
-	mlfw_mat_double_destroy(scaled_matrix);
+	dmlfw_mat_double_destroy(matrix);
+	dmlfw_row_vec_string_destroy(header);
+	dmlfw_mat_double_destroy(scaled_matrix);
 	return 0;
 }
 
