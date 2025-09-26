08297c7ce93a06e689e1c3dcd4a931d9b2c4968d
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:09:51 -0700

    Replace prefix 'mlfw' with 'dmlfw' in scale_using_parameters_file.c

--- tools/src/scale_using_parameters_file.c
+++ tools/src/scale_using_parameters_file.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-#include<mlfw.h>
+#include<dmlfw.h>
 int main(int argc,char *argv[])
 {
-	mlfw_mat_double *scaled_matrix=NULL;
-	mlfw_mat_double *matrix=NULL;
-	mlfw_row_vec_string *matrix_header=NULL;
+	dmlfw_mat_double *scaled_matrix=NULL;
+	dmlfw_mat_double *matrix=NULL;
+	dmlfw_row_vec_string *matrix_header=NULL;
 	char *algorithm;
 	char *parameters_file;
 	char *source;
@@ -20,25 +20,26 @@ int main(int argc,char *argv[])
 	target=argv[2];
 	algorithm=argv[3];
 	parameters_file=argv[4];
-	matrix=mlfw_mat_double_from_csv(source,NULL,&matrix_header);
-	if(mlfw_error()) goto err;
-	mlfw_mat_double_destroy(matrix);
+	matrix=dmlfw_mat_double_from_csv(source,NULL,&matrix_header);
+	if(dmlfw_error()) goto err;
+	dmlfw_mat_double_destroy(matrix);
 	matrix=NULL;
-	if(mlfw_error()) goto err;	
-	scaled_matrix=mlfw_scale_double_with_given_parameters(source,parameters_file,algorithm,NULL);
-	if(mlfw_error()) goto err;
+	if(dmlfw_error()) goto err;	
+	scaled_matrix=dmlfw_scale_double_with_given_parameters(source,parameters_file,algorithm,NULL);
+	if(dmlfw_error()) goto err;
 
-	mlfw_mat_double_to_csv(scaled_matrix,target,matrix_header);
-	
-	mlfw_mat_double_destroy(scaled_matrix);
-	mlfw_row_vec_string_destroy(matrix_header);
-	mlfw_mat_double_destroy(matrix);
+	dmlfw_mat_double_to_csv(scaled_matrix,target,matrix_header);
+	printf("%s file generated\n",target);
+	if(dmlfw_error()) goto err;	
+	dmlfw_mat_double_destroy(scaled_matrix);
+	dmlfw_row_vec_string_destroy(matrix_header);
+	dmlfw_mat_double_destroy(matrix);
 	return 0;
 err:
-	mlfw_get_error_string(error_string,512);
+	dmlfw_get_error_string(error_string,512);
 	printf("Error : %s\n",error_string);
-	mlfw_mat_double_destroy(scaled_matrix);
-	mlfw_row_vec_string_destroy(matrix_header);
-	mlfw_mat_double_destroy(matrix);
+	dmlfw_mat_double_destroy(scaled_matrix);
+	dmlfw_row_vec_string_destroy(matrix_header);
+	dmlfw_mat_double_destroy(matrix);
 	return 0;
 }
