6c7ab6370bce21d936e96d573c80a3b038b216ac
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:09:51 -0700

    Replace prefix 'mlfw' with 'dmlfw' in scale_dataset.c

--- tools/src/scale_dataset.c
+++ tools/src/scale_dataset.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-#include<mlfw.h>
+#include<dmlfw.h>
 int main(int argc,char *argv[])
 {
-	mlfw_mat_double *scaled_matrix=NULL;
-	mlfw_row_vec_string *columns_to_scale=NULL;
-	mlfw_mat_double *matrix=NULL;
-	mlfw_row_vec_string *matrix_header=NULL;
+	dmlfw_mat_double *scaled_matrix=NULL;
+	dmlfw_row_vec_string *columns_to_scale=NULL;
+	dmlfw_mat_double *matrix=NULL;
+	dmlfw_row_vec_string *matrix_header=NULL;
 	char *algorithm;
 	char *parameters_file;
 	char *source;
@@ -24,32 +24,32 @@ int main(int argc,char *argv[])
 	algorithm=argv[3];
 	parameters_file=argv[4];
 	columns_to_scale_size=argc-5; // -5 because (program_name,source,target,algorithm and parameters_csv)
-	matrix=mlfw_mat_double_from_csv(source,NULL,&matrix_header);
-	if(mlfw_error()) goto err;
-	mlfw_mat_double_destroy(matrix);
+	matrix=dmlfw_mat_double_from_csv(source,NULL,&matrix_header);
+	if(dmlfw_error()) goto err;
+	dmlfw_mat_double_destroy(matrix);
 	matrix=NULL;
-	columns_to_scale=mlfw_row_vec_string_create_new(columns_to_scale_size);
-	if(mlfw_error()) goto err;	
+	columns_to_scale=dmlfw_row_vec_string_create_new(columns_to_scale_size);
+	if(dmlfw_error()) goto err;	
 	for(i=5;i<argc;++i)
 	{
-		mlfw_row_vec_string_set(columns_to_scale,i-5,argv[i]);
+		dmlfw_row_vec_string_set(columns_to_scale,i-5,argv[i]);
 	}
-	scaled_matrix=mlfw_scale_double(source,columns_to_scale,parameters_file,algorithm,NULL);
-	if(mlfw_error()) goto err;
+	scaled_matrix=dmlfw_scale_double(source,columns_to_scale,parameters_file,algorithm,NULL);
+	if(dmlfw_error()) goto err;
 
-	mlfw_mat_double_to_csv(scaled_matrix,target,matrix_header);
+	dmlfw_mat_double_to_csv(scaled_matrix,target,matrix_header);
 	
-	mlfw_row_vec_string_destroy(columns_to_scale);
-	mlfw_mat_double_destroy(scaled_matrix);
-	mlfw_row_vec_string_destroy(matrix_header);
-	mlfw_mat_double_destroy(matrix);
+	dmlfw_row_vec_string_destroy(columns_to_scale);
+	dmlfw_mat_double_destroy(scaled_matrix);
+	dmlfw_row_vec_string_destroy(matrix_header);
+	dmlfw_mat_double_destroy(matrix);
 	return 0;
 err:
-	mlfw_get_error_string(error_string,512);
+	dmlfw_get_error_string(error_string,512);
 	printf("Error : %s\n",error_string);
-	mlfw_row_vec_string_destroy(columns_to_scale);
-	mlfw_mat_double_destroy(scaled_matrix);
-	mlfw_row_vec_string_destroy(matrix_header);
-	mlfw_mat_double_destroy(matrix);
+	dmlfw_row_vec_string_destroy(columns_to_scale);
+	dmlfw_mat_double_destroy(scaled_matrix);
+	dmlfw_row_vec_string_destroy(matrix_header);
+	dmlfw_mat_double_destroy(matrix);
 	return 0;
 }
