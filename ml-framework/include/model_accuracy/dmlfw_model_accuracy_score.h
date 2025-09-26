cbfbe1eb12d4572cc3cb320aa4b6b78be9fd79fa
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 19:49:26 -0700

    Renamed prefix mlfw to dmlfw

---
+++ ml-framework/include/model_accuracy/dmlfw_model_accuracy_score.h
@@ -0,0 +1,5 @@
+#ifndef __DMLFW__MODEL__ACCURACY__SCORE__
+#define __DMLFW__MODEL__ACCURACY__SCORE__
+#include<dmlfw_vector.h>
+double dmlfw_get_r2_score(dmlfw_column_vec_double *target_values_vector,dmlfw_column_vec_double *predicted_values_vector);
+#endif
