262c22c8072c93531b063755f2550f7a2aa9c11c
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 19:49:26 -0700

    Renamed prefix mlfw to dmlfw

---
+++ ml-framework/include/old/dmlfw_learning.h
@@ -0,0 +1,15 @@
+#ifndef __DMLFW__LEARNING__
+#define __DMLFW__LEARNING__
+#include<inttypes.h>
+
+
+#ifndef dimension_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+
+
+#include<learning/dmlfw_score.h>
+#include<learning/dmlfw_linear_regression.h>
+#include<learning/dmlfw_logistic_regression.h>
+#endif
