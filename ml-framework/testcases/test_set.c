82e1ce3f63b29224680066cb1031076fd9aeecac
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:03:36 -0700

    Replace mlfw with dmlfw in test_set.c

--- ml-framework/testcases/test_set.c
+++ ml-framework/testcases/test_set.c
@@ -1,33 +1,33 @@
 #include<stdio.h>
-#include<mlfw_error.h>
-#include<mlfw_set.h>
+#include<dmlfw_error.h>
+#include<dmlfw_set.h>
 int main()
 {
-	mlfw_set_string *s;
+	dmlfw_set_string *s;
 	char err[512];
 	char dbg[512];
-	s=mlfw_set_string_create_new();
-	if(mlfw_error())
+	s=dmlfw_set_string_create_new();
+	if(dmlfw_error())
 	{
 		printf("Unable to create set\n");
-		printf("Error code : %u\n",mlfw_get_error_code());
-		mlfw_get_error_string(err,512);
-		mlfw_get_debug_string(dbg,512);
+		printf("Error code : %u\n",dmlfw_get_error_code());
+		dmlfw_get_error_string(err,512);
+		dmlfw_get_debug_string(dbg,512);
 		printf("Error : %s\n",err);
 		printf("Debug : %s\n",dbg);
 		return 0;
 	}
-	mlfw_set_string_destroy(NULL);
-	if(mlfw_error())
+	dmlfw_set_string_destroy(NULL);
+	if(dmlfw_error())
 	{
 		printf("Unable to create set\n");
-		printf("Error code : %u\n",mlfw_get_error_code());
-		mlfw_get_error_string(err,512);
-		mlfw_get_debug_string(dbg,512);
+		printf("Error code : %u\n",dmlfw_get_error_code());
+		dmlfw_get_error_string(err,512);
+		dmlfw_get_debug_string(dbg,512);
 		printf("Error : %s\n",err);
 		printf("Debug : %s\n",dbg);
 		return 0;
 	}
-	mlfw_set_string_destroy(s);
+	dmlfw_set_string_destroy(s);
 	return 0;
 }
