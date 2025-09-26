60b95da0fbc96d78b082353129851ea896563d44
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:03:26 -0700

    Replace mlfw with dmlfw in test_header_from_csv.c

--- ml-framework/testcases/module-2/test_header_from_csv.c
+++ ml-framework/testcases/module-2/test_header_from_csv.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<mlfw_vector.h>
-#include<mlfw_matrix.h>
+#include<dmlfw_vector.h>
+#include<dmlfw_matrix.h>
 int main()
 {
-	mlfw_row_vec_string *header;
-	mlfw_mat_double *matrix;
+	dmlfw_row_vec_string *header;
+	dmlfw_mat_double *matrix;
 	char *ptr;
 	double value;
 	index_t r,c,i;
 	dimension_t size,rows,columns;
-	matrix=mlfw_mat_double_from_csv("IceCreamSales.csv",NULL,&header);
+	matrix=dmlfw_mat_double_from_csv("IceCreamSales.csv",NULL,&header);
 	if(matrix==NULL)
 	{
 		printf("Unable to load the dataset\n");
 		return 0;
 	}
-	size=mlfw_row_vec_string_get_size(header);
-	mlfw_mat_double_get_dimensions(matrix,&rows,&columns);
+	size=dmlfw_row_vec_string_get_size(header);
+	dmlfw_mat_double_get_dimensions(matrix,&rows,&columns);
 	for(i=0;i<size;++i)
 	{
-		mlfw_row_vec_string_get(header,i,&ptr);
+		dmlfw_row_vec_string_get(header,i,&ptr);
 		if(ptr!=NULL)
 		{
 			printf("%s ",ptr);
@@ -32,12 +32,12 @@ int main()
 	{
 		for(c=0;c<columns;++c)
 		{
-			value=mlfw_mat_double_get(matrix,r,c);
+			value=dmlfw_mat_double_get(matrix,r,c);
 			printf("%lf ",value);		
 		}
 		printf("\n");
 	}
-	mlfw_row_vec_string_destroy(header);
-	mlfw_mat_double_destroy(matrix);
+	dmlfw_row_vec_string_destroy(header);
+	dmlfw_mat_double_destroy(matrix);
 	return 0;
 }
