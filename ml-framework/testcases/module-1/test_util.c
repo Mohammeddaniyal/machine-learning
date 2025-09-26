afbb2ccad0a06618068516f85eca69ed87361bbc
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:03:15 -0700

    Replace mlfw with dmlfw in test_util.c

--- ml-framework/testcases/module-1/test_util.c
+++ ml-framework/testcases/module-1/test_util.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-#include<mlfw_utils.h>
+#include<dmlfw_utils.h>
 int main(int argc,char *argv[])
 {
-	printf("%d\n",mlfw_strcmp_case_insensitive(argv[1],argv[2]));
+	printf("%d\n",dmlfw_strcmp_case_insensitive(argv[1],argv[2]));
 	return 0;
 }
