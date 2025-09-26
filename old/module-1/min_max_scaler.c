c69ec9bdb0c8d4d0f3242a5356c1c1626b9a0b2c
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:12:29 -0700

    Replace prefix 'mlfw' with 'dmlfw' in min_max_scaler.c

--- old/module-1/min_max_scaler.c
+++ old/module-1/min_max_scaler.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<mlfw_scale.h>
-#include<mlfw_matrix.h>
+#include<dmlfw_scale.h>
+#include<dmlfw_matrix.h>
 int main(int argc,char *argv[])
 {
-	mlfw_mat_double *matrix;
-	mlfw_mat_double *scaled_matrix;
+	dmlfw_mat_double *matrix;
+	dmlfw_mat_double *scaled_matrix;
 	dimension_t matrix_rows,matrix_columns;
 
 	char *input_file;
@@ -24,26 +24,26 @@ int main(int argc,char *argv[])
 	to_column_index=atoi(argv[4]);
 	min_max_file=argv[5];
 
-	matrix=mlfw_mat_double_from_csv(input_file);
+	matrix=dmlfw_mat_double_from_csv(input_file);
 	if(matrix==NULL)
 	{
 		printf("Low memory\n");
 		return 0;
 	}
-	mlfw_mat_double_get_dimensions(matrix,&matrix_rows,&matrix_columns);
+	dmlfw_mat_double_get_dimensions(matrix,&matrix_rows,&matrix_columns);
 	
-	scaled_matrix=mlfw_scale_double_min_max(matrix,0,from_column_index,matrix_rows-1,to_column_index,min_max_file);
+	scaled_matrix=dmlfw_scale_double_min_max(matrix,0,from_column_index,matrix_rows-1,to_column_index,min_max_file);
 	if(scaled_matrix==NULL)
 	{
 		printf("Low memory, while creating scaled matrix\n");
-		mlfw_mat_double_destroy(matrix);
+		dmlfw_mat_double_destroy(matrix);
 		return 0;
 	}
 	
-	mlfw_mat_double_to_csv(scaled_matrix,output_file);
+	dmlfw_mat_double_to_csv(scaled_matrix,output_file);
 		
-	mlfw_mat_double_destroy(matrix);
-	mlfw_mat_double_destroy(scaled_matrix);
+	dmlfw_mat_double_destroy(matrix);
+	dmlfw_mat_double_destroy(scaled_matrix);
 	return 0;
 }
 
