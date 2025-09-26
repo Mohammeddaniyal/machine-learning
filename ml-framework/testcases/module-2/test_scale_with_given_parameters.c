29c02bffaa0cf581fd92188e6df5b4dfc18edd1b
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:03:26 -0700

    Replace mlfw with dmlfw in test_scale_with_given_parameters.c

--- ml-framework/testcases/module-2/test_scale_with_given_parameters.c
+++ ml-framework/testcases/module-2/test_scale_with_given_parameters.c
@@ -1,31 +1,31 @@
 #include<stdio.h>
-#include<mlfw_matrix.h>
-#include<mlfw_scale.h>
-#include<mlfw_vector.h>
+#include<dmlfw_matrix.h>
+#include<dmlfw_scale.h>
+#include<dmlfw_vector.h>
 int main()
 {
-	mlfw_mat_double *scaled_matrix;
-	mlfw_row_vec_string *matrix_header;
-	scaled_matrix=mlfw_scale_double_with_given_parameters("dummy.csv","mean_sd.csv","z-score",NULL);
+	dmlfw_mat_double *scaled_matrix;
+	dmlfw_row_vec_string *matrix_header;
+	scaled_matrix=dmlfw_scale_double_with_given_parameters("dummy.csv","mean_sd.csv","z-score",NULL);
 	if(scaled_matrix==NULL)
 	{
 		printf("Low memory or some other reason, scaling not done\n");
 		return 0;
 	}
-	matrix_header=mlfw_row_vec_string_create_new(4);
+	matrix_header=dmlfw_row_vec_string_create_new(4);
 	if(matrix_header==NULL)
 	{
 		printf("Low memory\n");
-		mlfw_mat_double_destroy(scaled_matrix);
+		dmlfw_mat_double_destroy(scaled_matrix);
 		return 0;
 	}
-	mlfw_row_vec_string_set(matrix_header,0,"age");
-	mlfw_row_vec_string_set(matrix_header,1,"height");
-	mlfw_row_vec_string_set(matrix_header,2,"salary");
-	mlfw_row_vec_string_set(matrix_header,3,"incentive");
-	mlfw_mat_double_to_csv(scaled_matrix,"scaled_dummy_with_given_parameters.csv",matrix_header);
+	dmlfw_row_vec_string_set(matrix_header,0,"age");
+	dmlfw_row_vec_string_set(matrix_header,1,"height");
+	dmlfw_row_vec_string_set(matrix_header,2,"salary");
+	dmlfw_row_vec_string_set(matrix_header,3,"incentive");
+	dmlfw_mat_double_to_csv(scaled_matrix,"scaled_dummy_with_given_parameters.csv",matrix_header);
 	
-	mlfw_mat_double_destroy(scaled_matrix);
-	mlfw_row_vec_string_destroy(matrix_header);
+	dmlfw_mat_double_destroy(scaled_matrix);
+	dmlfw_row_vec_string_destroy(matrix_header);
 	return 0;
 }
