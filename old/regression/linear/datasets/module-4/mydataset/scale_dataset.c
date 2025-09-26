d7d21b5723e4dd27da30d1c225b2dc00e167c84e
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:12:29 -0700

    Replace prefix 'mlfw' with 'dmlfw' in scale_dataset.c

--- old/regression/linear/datasets/module-4/mydataset/scale_dataset.c
+++ old/regression/linear/datasets/module-4/mydataset/scale_dataset.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
-#include<mlfw_matrix.h>
-#include<mlfw_scale.h>
-#include<mlfw_vector.h>
+#include<dmlfw_matrix.h>
+#include<dmlfw_scale.h>
+#include<dmlfw_vector.h>
 int main(int argc,char *argv[])
 {
-	mlfw_mat_double *scaled_matrix;
-	mlfw_row_vec_string *columns_to_scale;
-	mlfw_mat_double *matrix;
-	mlfw_row_vec_string *matrix_header;
+	dmlfw_mat_double *scaled_matrix;
+	dmlfw_row_vec_string *columns_to_scale;
+	dmlfw_mat_double *matrix;
+	dmlfw_row_vec_string *matrix_header;
 	char *source;
 	char *target;
 	index_t i;
@@ -20,38 +20,38 @@ int main(int argc,char *argv[])
 	}
 	source=argv[1];
 	target=argv[2];
-	matrix=mlfw_mat_double_from_csv(source,NULL,&matrix_header);
+	matrix=dmlfw_mat_double_from_csv(source,NULL,&matrix_header);
 	if(matrix==NULL)
 	{
 		printf("Unable to load %s\n",source);
 		return 0;
 	}
-	mlfw_mat_double_destroy(matrix);
+	dmlfw_mat_double_destroy(matrix);
 	columns_to_scale_size=argc-3; // -3 because (program_name,source,target)
-	columns_to_scale=mlfw_row_vec_string_create_new(columns_to_scale_size);
+	columns_to_scale=dmlfw_row_vec_string_create_new(columns_to_scale_size);
 	if(columns_to_scale==NULL)
 	{
 		printf("Low memory\n");
-		mlfw_row_vec_string_destroy(matrix_header);
+		dmlfw_row_vec_string_destroy(matrix_header);
 		return 0;
 	}
 	
 	for(i=3;i<argc;++i)
 	{
-		mlfw_row_vec_string_set(columns_to_scale,i-3,argv[i]);
+		dmlfw_row_vec_string_set(columns_to_scale,i-3,argv[i]);
 	}
-	scaled_matrix=mlfw_scale_double(source,columns_to_scale,"z_score.csv","z-score",NULL);
+	scaled_matrix=dmlfw_scale_double(source,columns_to_scale,"z_score.csv","z-score",NULL);
 	if(scaled_matrix==NULL)
 	{
 		printf("Low memory or some other reason, scaling not done\n");
-		mlfw_row_vec_string_destroy(columns_to_scale);
-		mlfw_row_vec_string_destroy(matrix_header);
+		dmlfw_row_vec_string_destroy(columns_to_scale);
+		dmlfw_row_vec_string_destroy(matrix_header);
 		return 0;
 	}
-	mlfw_mat_double_to_csv(scaled_matrix,target,matrix_header);
+	dmlfw_mat_double_to_csv(scaled_matrix,target,matrix_header);
 	
-	mlfw_row_vec_string_destroy(columns_to_scale);
-	mlfw_mat_double_destroy(scaled_matrix);
-	mlfw_row_vec_string_destroy(matrix_header);
+	dmlfw_row_vec_string_destroy(columns_to_scale);
+	dmlfw_mat_double_destroy(scaled_matrix);
+	dmlfw_row_vec_string_destroy(matrix_header);
 	return 0;
 }
