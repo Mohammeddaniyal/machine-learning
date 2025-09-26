02d27937bf8be42254c4466fc5b94721f3216b41
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 19:49:00 -0700

    renamed prefix from mlfw to dmlfw

---
+++ ml-framework/include/dmlfw.h
@@ -0,0 +1,17 @@
+#ifndef __DMLFW__ML__
+#define __DMLFW__ML__
+
+#include<dmlfw_types.h>
+#include<dmlfw_matrix.h>
+#include<dmlfw_operations.h>
+#include<dmlfw_vector.h>
+#include<dmlfw_encoder.h>
+#include<dmlfw_math.h>
+#include<dmlfw_utils.h>
+#include<dmlfw_error.h>
+#include<dmlfw_scale.h>
+#include<dmlfw_model_accuracy.h>
+#include<dmlfw_set.h>
+#include<dmlfw_list.h>
+#include<dmlfw_algorithms.h>
+#endif
