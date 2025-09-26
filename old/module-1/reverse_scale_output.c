2f6d5c6775bb64e080875d22e3afe95a09257608
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:12:29 -0700

    Replace prefix 'mlfw' with 'dmlfw' in reverse_scale_output.c

--- old/module-1/reverse_scale_output.c
+++ old/module-1/reverse_scale_output.c
@@ -1,4 +1,4 @@
-#include<mlfw_matrix.h>
+#include<dmlfw_matrix.h>
 #include<stdlib.h>
 #include<stdio.h>
 int main(int argc,char *argv[])
@@ -10,9 +10,9 @@ int main(int argc,char *argv[])
 	double max;
 	double scaled_value;
 	double value;
-	mlfw_mat_double *matrix;
+	dmlfw_mat_double *matrix;
 	dimension_t matrix_rows,matrix_columns;
-	mlfw_mat_double *min_max_matrix;
+	dmlfw_mat_double *min_max_matrix;
 	dimension_t min_max_rows,min_max_columns;
 	index_t r,c;
 	if(argc!=4)
@@ -25,41 +25,41 @@ int main(int argc,char *argv[])
 	output_file=argv[2];
 	min_max_file=argv[3];
 
-	matrix=mlfw_mat_double_from_csv(input_file);
+	matrix=dmlfw_mat_double_from_csv(input_file);
 	if(matrix==NULL)
 	{
 		printf("Low memory\n");
 		return 0;
 	}
-	mlfw_mat_double_get_dimensions(matrix,&matrix_rows,&matrix_columns);
-	min_max_matrix=mlfw_mat_double_from_csv(min_max_file);
+	dmlfw_mat_double_get_dimensions(matrix,&matrix_rows,&matrix_columns);
+	min_max_matrix=dmlfw_mat_double_from_csv(min_max_file);
 	if(min_max_matrix==NULL)
 	{
 		printf("Low memory\n");
-		mlfw_mat_double_destroy(matrix);
+		dmlfw_mat_double_destroy(matrix);
 		return 0;
 	}
-	mlfw_mat_double_get_dimensions(min_max_matrix,&min_max_rows,&min_max_columns);
+	dmlfw_mat_double_get_dimensions(min_max_matrix,&min_max_rows,&min_max_columns);
 	
 	
 	// last column is the output (min and max values)
 	// and we only care about the output not input columns
-	min=mlfw_mat_double_get(min_max_matrix,0,min_max_columns-1);
-	max=mlfw_mat_double_get(min_max_matrix,1,min_max_columns-1);
+	min=dmlfw_mat_double_get(min_max_matrix,0,min_max_columns-1);
+	max=dmlfw_mat_double_get(min_max_matrix,1,min_max_columns-1);
 
 
 	for(r=0;r<matrix_rows;++r)
 	{
 		for(c=matrix_columns-2;c<matrix_columns;++c)
 		{
-			scaled_value=mlfw_mat_double_get(matrix,r,c);
+			scaled_value=dmlfw_mat_double_get(matrix,r,c);
 			value=(scaled_value*(max-min))+min;
-			mlfw_mat_double_set(matrix,r,c,value);
+			dmlfw_mat_double_set(matrix,r,c,value);
 		}
 	}
-	mlfw_mat_double_to_csv(matrix,output_file);
-	mlfw_mat_double_destroy(matrix);
-	mlfw_mat_double_destroy(min_max_matrix);
+	dmlfw_mat_double_to_csv(matrix,output_file);
+	dmlfw_mat_double_destroy(matrix);
+	dmlfw_mat_double_destroy(min_max_matrix);
 	return 0;
 }
 
