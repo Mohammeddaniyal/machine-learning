aed596df1c8d40de6308f59c4c321066b6d4edda
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 19:49:25 -0700

    Renamed prefix mlfw to dmlfw

---
+++ ml-framework/include/dmlfw_operations.h
@@ -0,0 +1,10 @@
+#ifndef __DMLFW__OPERATIONS__
+#define __DMLFW__OPERATIONS__
+
+#include<dmlfw_types.h>
+
+#include<dmlfw_vector.h>
+#include<dmlfw_matrix.h>
+#include<operations/dmlfw_operations_double.h>
+
+#endif
