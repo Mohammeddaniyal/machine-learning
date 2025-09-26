9f137bcecf2576b60128f26e80d1cdc397573b7f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:03:15 -0700

    Replace mlfw with dmlfw in test_from_csv.c

--- ml-framework/testcases/module-1/test_from_csv.c
+++ ml-framework/testcases/module-1/test_from_csv.c
@@ -1,25 +1,25 @@
-#include<mlfw_matrix.h>
-#include<mlfw_vector.h>
+#include<dmlfw_matrix.h>
+#include<dmlfw_vector.h>
 #include<stdlib.h>
 #include<stdio.h>
 void test_from_csv()
 {
 	index_t r,c;
-	mlfw_mat_double *matrix;
+	dmlfw_mat_double *matrix;
 	dimension_t rows,columns;
 	printf("Test case for from csv\n");
-	matrix=mlfw_mat_double_from_csv("IceCreamSales.csv");
+	matrix=dmlfw_mat_double_from_csv("IceCreamSales.csv");
 	if(matrix==NULL)
 	{
 		printf("Unable to load csv file : IceCreamSales.csv\n");
 		return;
 	}
-	mlfw_mat_double_get_dimensions(matrix,&rows,&columns);
+	dmlfw_mat_double_get_dimensions(matrix,&rows,&columns);
 	for(r=0;r<rows;++r)
 	{
 		for(c=0;c<columns;++c)
 		{
-			printf("%20.10lf ",mlfw_mat_double_get(matrix,r,c));
+			printf("%20.10lf ",dmlfw_mat_double_get(matrix,r,c));
 		}
 		printf("\n");
 	}
