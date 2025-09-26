f129e407bc26cbdedbf06899626646d3c971b0e6
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:03:15 -0700

    Replace mlfw with dmlfw in test_create_training_test_data.c

--- ml-framework/testcases/module-1/test_create_training_test_data.c
+++ ml-framework/testcases/module-1/test_create_training_test_data.c
@@ -1,43 +1,43 @@
-#include<mlfw_matrix.h>
+#include<dmlfw_matrix.h>
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
-	mlfw_row_vec_string *header;
-	mlfw_mat_double *minor_matrix;
-	mlfw_mat_double *major_matrix;
+	dmlfw_row_vec_string *header;
+	dmlfw_mat_double *minor_matrix;
+	dmlfw_mat_double *major_matrix;
 
 	dimension_t rows,columns;
 
-	mlfw_mat_double_get_training_testing_data("IceCreamSales.csv",&major_matrix,&minor_matrix,20);
+	dmlfw_mat_double_get_training_testing_data("IceCreamSales.csv",&major_matrix,&minor_matrix,20);
 	printf("hi\n");
 	if(minor_matrix==NULL || major_matrix==NULL)
 	{
 		printf("Unable to create testing & training datasets\n");
 		return 0;
 	}
-	mlfw_mat_double_get_dimensions(minor_matrix,&rows,&columns);
+	dmlfw_mat_double_get_dimensions(minor_matrix,&rows,&columns);
 	printf("%d,%d\n",rows,columns);
-	mlfw_mat_double_get_dimensions(major_matrix,&rows,&columns);
+	dmlfw_mat_double_get_dimensions(major_matrix,&rows,&columns);
 	printf("%d,%d\n",rows,columns);
-	header=mlfw_row_vec_string_create_new(2);
+	header=dmlfw_row_vec_string_create_new(2);
 	if(header==NULL)
 	{
 		printf("Low memory\n");
 		return 0;
 	}
 	
-	mlfw_row_vec_string_set(header,0,"temperature");
-	mlfw_row_vec_string_set(header,1,"profit");
+	dmlfw_row_vec_string_set(header,0,"temperature");
+	dmlfw_row_vec_string_set(header,1,"profit");
 	printf("Nice\n");
-	mlfw_mat_double_to_csv(minor_matrix,"testing_data.csv",header);
+	dmlfw_mat_double_to_csv(minor_matrix,"testing_data.csv",header);
 	printf("Dog\n");
-	mlfw_mat_double_to_csv(major_matrix,"training_data.csv",header);
+	dmlfw_mat_double_to_csv(major_matrix,"training_data.csv",header);
 	
 	printf("DONE\n");
 
-	mlfw_row_vec_string_destroy(header);
-	mlfw_mat_double_destroy(minor_matrix);
-	mlfw_mat_double_destroy(major_matrix);
+	dmlfw_row_vec_string_destroy(header);
+	dmlfw_mat_double_destroy(minor_matrix);
+	dmlfw_mat_double_destroy(major_matrix);
 	return 0;
 }
