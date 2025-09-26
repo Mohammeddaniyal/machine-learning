3c0223bcacf690655574673bfd4b86dae76d115d
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:09:50 -0700

    Replace prefix 'mlfw' with 'dmlfw' in column_cutter.c

--- tools/src/column_cutter.c
+++ tools/src/column_cutter.c
@@ -1,15 +1,15 @@
-#include<mlfw_matrix.h>
-#include<mlfw_vector.h>
-#include<mlfw_utils.h>
+#include<dmlfw_matrix.h>
+#include<dmlfw_vector.h>
+#include<dmlfw_utils.h>
 #include<stdio.h>
 #include<stdlib.h>
 int main(int argc,char *argv[])
 {
 	char *str;
-	mlfw_mat_string *matrix;
-	mlfw_mat_string *m;
-	mlfw_row_vec_string *matrix_header;
-	mlfw_row_vec_string *m_header;
+	dmlfw_mat_string *matrix;
+	dmlfw_mat_string *m;
+	dmlfw_row_vec_string *matrix_header;
+	dmlfw_row_vec_string *m_header;
 	dimension_t indexes_size;
 	index_t *indexes;
 	dimension_t matrix_header_size;
@@ -21,7 +21,7 @@ int main(int argc,char *argv[])
 		printf("Usage [column_cutter source_csv target_csv column_to_cut_1 column_to_cut_2 ....]\n");
 		return 0;
 	}
-	matrix=mlfw_mat_string_from_csv(argv[1],NULL,&matrix_header);
+	matrix=dmlfw_mat_string_from_csv(argv[1],NULL,&matrix_header);
 	if(matrix==NULL)
 	{
 		printf("Unable to load %s\n",argv[1]);
@@ -32,27 +32,27 @@ int main(int argc,char *argv[])
 	if(indexes==NULL)
 	{
 		printf("Low memory\n");
-		mlfw_mat_string_destroy(matrix);
-		mlfw_row_vec_string_destroy(matrix_header);
+		dmlfw_mat_string_destroy(matrix);
+		dmlfw_row_vec_string_destroy(matrix_header);
 		return 0;
 	}
-	matrix_header_size=mlfw_row_vec_string_get_size(matrix_header);
+	matrix_header_size=dmlfw_row_vec_string_get_size(matrix_header);
 	i=3;
 	k=0;
 	while(i<argc)
 	{
 		for(j=0;j<matrix_header_size;++j)
 		{
-			mlfw_row_vec_string_get(matrix_header,j,&str);
+			dmlfw_row_vec_string_get(matrix_header,j,&str);
 			if(str==NULL)
 			{
 				printf("Low memory\n");
-				mlfw_mat_string_destroy(matrix);
-				mlfw_row_vec_string_destroy(matrix_header);
+				dmlfw_mat_string_destroy(matrix);
+				dmlfw_row_vec_string_destroy(matrix_header);
 				free(indexes);
 				return 0;
 			}
-			if(mlfw_strcmp_case_insensitive(argv[i],str)==0)
+			if(dmlfw_strcmp_case_insensitive(argv[i],str)==0)
 			{
 				free(str);
 				indexes[k]=j;
@@ -64,33 +64,33 @@ int main(int argc,char *argv[])
 		if(j==matrix_header_size) //argv[i] not in matrix_header
 		{
 			printf("%s column does not exists\n",argv[i]);
-			mlfw_mat_string_destroy(matrix);
-			mlfw_row_vec_string_destroy(matrix_header);
+			dmlfw_mat_string_destroy(matrix);
+			dmlfw_row_vec_string_destroy(matrix_header);
 			free(indexes);
 			return 0;
 		}
 		i++;
 	}
 
-	m=mlfw_mat_string_delete_columns(matrix,indexes,indexes_size,NULL);
+	m=dmlfw_mat_string_delete_columns(matrix,indexes,indexes_size,NULL);
 	if(m==NULL)
 	{
 		printf("Low memory\n");
 		free(indexes);
-		mlfw_mat_string_destroy(matrix);
-		mlfw_row_vec_string_destroy(matrix_header);
+		dmlfw_mat_string_destroy(matrix);
+		dmlfw_row_vec_string_destroy(matrix_header);
 		return 0;
 	}
-	mlfw_mat_string_destroy(matrix);
+	dmlfw_mat_string_destroy(matrix);
 	
 	m_header_size=matrix_header_size-indexes_size;
-	m_header=mlfw_row_vec_string_create_new(m_header_size);
+	m_header=dmlfw_row_vec_string_create_new(m_header_size);
 	if(m_header==NULL)
 	{	
 		printf("Low memory\n");
 		free(indexes);
-		mlfw_row_vec_string_destroy(matrix_header);
-		mlfw_mat_string_destroy(m);
+		dmlfw_row_vec_string_destroy(matrix_header);
+		dmlfw_mat_string_destroy(m);
 		return 0;
 	}	
 	
@@ -103,24 +103,24 @@ int main(int argc,char *argv[])
 			if(indexes[j]==i) break; //found the column index in indexes, which needs to be ignored
 		}
 		if(j<indexes_size) continue; // ignore this column
-		mlfw_row_vec_string_get(matrix_header,i,&str);
+		dmlfw_row_vec_string_get(matrix_header,i,&str);
 		if(str==NULL)
 		{
 			printf("Low memory\n");
 			free(indexes);
-			mlfw_mat_string_destroy(m);
-			mlfw_row_vec_string_destroy(m_header);
-			mlfw_row_vec_string_destroy(matrix_header);
+			dmlfw_mat_string_destroy(m);
+			dmlfw_row_vec_string_destroy(m_header);
+			dmlfw_row_vec_string_destroy(matrix_header);
 			return 0;
 		}
-		mlfw_row_vec_string_set(m_header,k,str);
+		dmlfw_row_vec_string_set(m_header,k,str);
 		k++;
 		free(str);
 	}
-	mlfw_mat_string_to_csv(m,argv[2],m_header);
+	dmlfw_mat_string_to_csv(m,argv[2],m_header);
 	free(indexes);
-	mlfw_mat_string_destroy(m);
-	mlfw_row_vec_string_destroy(m_header);
-	mlfw_row_vec_string_destroy(matrix_header);
+	dmlfw_mat_string_destroy(m);
+	dmlfw_row_vec_string_destroy(m_header);
+	dmlfw_row_vec_string_destroy(matrix_header);
 	return 0;
 }
