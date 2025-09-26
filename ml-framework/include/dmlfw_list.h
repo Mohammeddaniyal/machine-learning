767dd33483e858be81a1c7f675d0144e9cbd4bdd
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 19:49:25 -0700

    Renamed prefix mlfw to dmlfw

---
+++ ml-framework/include/dmlfw_list.h
@@ -0,0 +1,31 @@
+/**
+ * @file dmlfw_list.h
+ * @brief Unified interface for list data structures.
+ *
+ * This umbrella header consolidates list types, providing access to core functionalities such as:
+ * - Creation and destruction
+ * - Element insertion and retrieval
+ * - Conversion to vector forms
+ *
+ * The module currently supports double precision forward list types, with their operations defined in respective headers.
+ *
+ * The list module offers lightweight, dynamic linear collections suitable for various numerical and data handling tasks within the framework.
+ *
+ * @defgroup list List Module
+ * @brief Core list types and utilities for double precision data.
+ *
+ * See also the specialized headers for detailed APIs:
+ * - @ref dmlfw_list_double.h "Double Forward List"
+ *
+ * @{
+ */
+
+#ifndef __DMLFW__LIST__
+#define __DMLFW__LIST__
+
+#include <dmlfw_types.h>
+#include <list/dmlfw_list_double.h>
+
+#endif // __DMLFW__LIST__
+
+/** @} */
