6000eeeccbc1fef0e39d75a426ee9a11ef0ba37e
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 19:49:26 -0700

    Renamed prefix mlfw to dmlfw

---
+++ ml-framework/include/old/learning/dmlfw_score.h
@@ -0,0 +1,6 @@
+#ifndef __DMLFW__SCORE__
+#define __DMLFW__SCORE__
+#include<dmlfw_vector.h>
+double dmlfw_get_r2_score(dmlfw_column_vec_double *target_values_vector,dmlfw_column_vec_double *predicted_values_vector);
+
+#endif
