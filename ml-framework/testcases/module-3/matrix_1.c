1ea4504a829a7634ad3b6b1e9bf2e3bcaaa8bb4b
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 20:03:30 -0700

    Replace mlfw with dmlfw in matrix_1.c

--- ml-framework/testcases/module-3/matrix_1.c
+++ ml-framework/testcases/module-3/matrix_1.c
@@ -1,26 +1,26 @@
-#include<mlfw_matrix.h>
-#include<mlfw_operations.h>
+#include<dmlfw_matrix.h>
+#include<dmlfw_operations.h>
 #include<stdio.h>
 int main()
 {
 	int x[2][2]={{10,20},{30,40}};
 	int y[2][2]={{1,2},{3,4}};
 	
-	mlfw_mat_double *m1,*m2,*m3;
+	dmlfw_mat_double *m1,*m2,*m3;
 	dimension_t rows=2;
 	dimension_t columns=2;
 	index_t r,c;
 
-	m1=mlfw_mat_double_create_new(rows,columns);
+	m1=dmlfw_mat_double_create_new(rows,columns);
 	if(m1==NULL)
 	{
 		printf("Low memory\n");
 		return 0;
 	}
-	m2=mlfw_mat_double_create_new(rows,columns);
+	m2=dmlfw_mat_double_create_new(rows,columns);
 	if(m2==NULL)
 	{
-		mlfw_mat_double_destroy(m1);
+		dmlfw_mat_double_destroy(m1);
 		return 0;
 	}
 	
@@ -28,17 +28,17 @@ int main()
 	{
 		for(c=0;c<columns;++c)
 		{
-			mlfw_mat_double_set(m1,r,c,x[r][c]);
-			mlfw_mat_double_set(m2,r,c,y[r][c]);
+			dmlfw_mat_double_set(m1,r,c,x[r][c]);
+			dmlfw_mat_double_set(m2,r,c,y[r][c]);
 		}
 	}
 
-	m3=mlfw_multiply_double_matrix_with_matrix(m1,m2,NULL);
+	m3=dmlfw_multiply_double_matrix_with_matrix(m1,m2,NULL);
 	if(m3==NULL)
 	{
 		printf("Low memory\n");
-		mlfw_mat_double_destroy(m1);
-		mlfw_mat_double_destroy(m2);
+		dmlfw_mat_double_destroy(m1);
+		dmlfw_mat_double_destroy(m2);
 		return 0;
 	}
 	
@@ -46,13 +46,13 @@ int main()
 	{
 		for(c=0;c<columns;++c)
 		{
-			printf("%lf ",mlfw_mat_double_get(m3,r,c));
+			printf("%lf ",dmlfw_mat_double_get(m3,r,c));
 		}
 		printf("\n");
 	}
 
-	mlfw_mat_double_destroy(m1);
-	mlfw_mat_double_destroy(m2);
-	mlfw_mat_double_destroy(m3);
+	dmlfw_mat_double_destroy(m1);
+	dmlfw_mat_double_destroy(m2);
+	dmlfw_mat_double_destroy(m3);
 	return 0;
 }
