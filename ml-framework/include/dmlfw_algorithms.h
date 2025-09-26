0e547f07159f7d540f484a85d6b126cef1334256
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-09-25 19:49:25 -0700

    Renamed prefix mlfw to dmlfw

---
+++ ml-framework/include/dmlfw_algorithms.h
@@ -0,0 +1,9 @@
+#ifndef __DMLFW__ALGORITHM__
+#define __DMLFW__ALGORITHM__
+
+#include<dmlfw_types.h>
+
+#include<ml_algorithms/dmlfw_ml_configurations.h>
+#include<ml_algorithms/dmlfw_linear_regression.h>
+
+#endif
